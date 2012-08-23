0d84366a919a616f18c68c8c97814433880c9096
Remigiusz.Boguszewicz <Remigiusz.Boguszewicz@1a1a0f9a-0ea6-11df-9e6e-d17b0969bdea>
2012-08-23 20:15:44 +0000

    a

--- oi_musas_awr/prepare_exec_cmd.sh
+++ oi_musas_awr/prepare_exec_cmd.sh
@@ -2,8 +2,6 @@
 #$Id$
 #
 # Prepare the command line to look like:
-#11 16 * * * . ${HOME}/.bash_profile; ${HOME}/oi_musas_awr/awr_reports.sh TEST `date -I` 08:00 16:00 > /tmp/awr_reports.log
-#21 16 * * * . ${HOME}/.bash_profile; ${HOME}/oi_musas_awr/awr_reports.sh TEST2 `date -I` 08:00 16:00 > /tmp/awr_reports.log
-#31 16 * * * . ${HOME}/.bash_profile; ${HOME}/oi_musas_awr/awr_reports.sh ZEBRA `date -I` 08:00 16:00 > /tmp/awr_reports.log
+#${HOME}/oi_musas_awr/awr_reports.sh TEST perfstat `date -I` 14:00 15:00
 
 $HOME/scripto/perl/ask_ldap.pl "orainfDbMusasMonitoring=TRUE" "['cn','orainfDbRrdoraUser','orainfDbMusasStart','orainfDbMusasEnd']" | awk '{ print "${HOME}/oi_musas_awr/awr_reports.sh " $1 " " $2 " `date -I` " $3 " " $4 " > /tmp/awr_reports.log" }' 
