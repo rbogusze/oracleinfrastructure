d53b5856ba50fd826a3a4299929b79b58b5909a8
Remigiusz.Boguszewicz <Remigiusz.Boguszewicz@1a1a0f9a-0ea6-11df-9e6e-d17b0969bdea>
2012-08-23 11:16:54 +0000

    a

--- oi_musas_awr/prepare_exec_cmd.sh
+++ oi_musas_awr/prepare_exec_cmd.sh
@@ -6,7 +6,4 @@
 #21 16 * * * . ${HOME}/.bash_profile; ${HOME}/oi_musas_awr/awr_reports.sh TEST2 `date -I` 08:00 16:00 > /tmp/awr_reports.log
 #31 16 * * * . ${HOME}/.bash_profile; ${HOME}/oi_musas_awr/awr_reports.sh ZEBRA `date -I` 08:00 16:00 > /tmp/awr_reports.log
 
-
-#5 21 * * 1-5 $HOME/scripto/perl/ask_ldap.pl "remikDbMusasMonitoring=TRUE" "['cn', 'remikDbMusasStart', 'remikDbMusasEnd']" | awk '{ print "/home/logwatch/musas/get_statspack.sh "$1 " perfhal `date -I`--" $2 " `date -I`--" $3 " >/dev/null 2>&1"}' > /home/logwatch/musas/musas_commands_from_ldap; sh /home/logwatch/musas/musas_commands_from_ldap
-
-$HOME/scripto/perl/ask_ldap.pl "orainfMusasMonitoring=TRUE" "['cn']" | awk '{ print "${HOME}/oi_musas_awr/awr_reports.sh " $1 " `date -I` 08:00 16:00 > /tmp/awr_reports.log" }' 
+$HOME/scripto/perl/ask_ldap.pl "orainfDbMusasMonitoring=TRUE" "['cn']" | awk '{ print "${HOME}/oi_musas_awr/awr_reports.sh " $1 " `date -I` 08:00 16:00 > /tmp/awr_reports.log" }' 
