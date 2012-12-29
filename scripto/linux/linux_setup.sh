35cfce781de69ef6596b849d49268a9f7b1b3811
Remigiusz.Boguszewicz <Remigiusz.Boguszewicz@gmail.com>
2012-12-29 18:08:15 +0000

    a

--- scripto/linux/linux_setup.sh
+++ scripto/linux/linux_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$Id: _base_script_block.sh,v 1.1 2012-05-07 13:47:27 remik Exp $
 
-LOG_DIR=/var/tmp/prepare_to_failover
+LOG_DIR=/var/tmp/linux_setup
 mkdir -p $LOG_DIR
 
 (. ./prepare_to_failover.wrap 2>&1) | tee $LOG_DIR/prepare_to_failover_log_$1.`date '+%Y-%m-%d--%H:%M:%S'`
