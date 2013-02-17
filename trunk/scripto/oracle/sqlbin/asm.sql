8b332b38fde766e28d08496edabeaecdd530b03b
Remigiusz.Boguszewicz <Remigiusz.Boguszewicz@1a1a0f9a-0ea6-11df-9e6e-d17b0969bdea>
2013-02-17 15:37:09 +0000

    a

--- trunk/scripto/oracle/sqlbin/asm.sql
+++ trunk/scripto/oracle/sqlbin/asm.sql
@@ -1,2 +1,2 @@
 COL % FORMAT 99.0
-SELECT name, free_mb, total_mb, free_mb/total_mb*100 "%FREE" FROM v$asm_diskgroup;
+SELECT name, free_mb, total_mb, round(free_mb/total_mb*100) "%FREE" FROM v$asm_diskgroup;
