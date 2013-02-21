5ff76fec86c7f3216e6f682a127f0730b350edcf
Remigiusz.Boguszewicz@gmail.com <Remigiusz.Boguszewicz@gmail.com@1a1a0f9a-0ea6-11df-9e6e-d17b0969bdea>
2013-02-21 12:03:37 +0000

    a

--- trunk/scripto/oracle/sqlbin/jobs.sql
+++ trunk/scripto/oracle/sqlbin/jobs.sql
@@ -1,7 +1,8 @@
 prompt Show some info about the jobs
 alter session set NLS_DATE_FORMAT = "YYYY/MM/DD HH24:MI:SS";
 column what format a40
-set lines 100
+column LOG_USER format a10
+set lines 200
 set pagesize 100
-select JOB, WHAT, LAST_DATE, NEXT_DATE, BROKEN from dba_jobs 
+select JOB, LOG_USER, WHAT, LAST_DATE, NEXT_DATE, BROKEN from dba_jobs 
 order by LAST_DATE;
