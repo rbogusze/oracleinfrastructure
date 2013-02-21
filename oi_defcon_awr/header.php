65caf2b0e5b60d113ee33292d15d7e3005f2110a
Remigiusz.Boguszewicz <Remigiusz.Boguszewicz@1a1a0f9a-0ea6-11df-9e6e-d17b0969bdea>
2013-02-21 17:42:38 +0000

    a

---
+++ oi_defcon_awr/header.php
@@ -0,0 +1,13 @@
+<?php
+echo "
+<html>
+<head>
+  <title>Defcon1x</title>
+  <meta http-equiv=\"content-type\"
+   content=\"text/html; charset=ISO-8859-2\">
+  <LINK REL=StyleSheet HREF=\"style.css\" TYPE=\"text/css\" MEDIA=screen>
+
+</head>
+<body>
+";
+?>
---
+++ oi_defcon_awr/show_file.php
@@ -0,0 +1,54 @@
+<?php
+/* $Id: show_file.php,v 1.1 2012-05-17 10:25:16 orainf Exp $ */
+//require("header.php");
+
+$filename=$_GET['filename'];
+$syntax_highlight=$_GET['syntax_highlight'];
+
+// There are two ways to do it
+
+// 1. Use shell utility
+//$tmp = shell_exec("txt2html $filename");
+//echo $tmp;
+
+// 2. Or do it yourself
+
+// sanity check
+if (is_file($filename) ) {
+  // open file
+  $fh = fopen ($filename, "r") or die("Could not open file");
+  // read file
+  echo "<pre>";
+  while (!feof($fh))
+  {
+    $data = fgets($fh);
+    // remove FF char
+    $data = str_replace("\f",'',$data);
+
+    // Highlight full scans
+    if (strstr ( $data, "FULL" ) ) { $highlight = "<font color='red'>"; }
+    // Highlight what was requested in URL
+    if (strstr ( $data, $syntax_highlight ) ) { $highlight = "<font color='blue'>"; }
+   
+    // If there is something to highlight do it
+    if ( $highlight ) { 
+      echo $highlight; 
+      $highlight = ""; 
+      echo $data . "</font>";
+    } else {
+      echo $data;
+    } 
+  } // while (!feof($fh))
+
+  // close file
+  fclose ($fh);
+  echo "</pre>";
+} else {
+  echo "File could not be found.\n";
+  exit(1);
+}
+
+
+//Include footer file with navigation links
+//require("footer.php");
+?>
