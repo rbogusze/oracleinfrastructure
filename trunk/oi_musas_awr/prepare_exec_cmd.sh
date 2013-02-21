7d3d6efb096f606f6365758a7f7030120593e1d2
Remigiusz.Boguszewicz <Remigiusz.Boguszewicz@1a1a0f9a-0ea6-11df-9e6e-d17b0969bdea>
2013-02-21 16:24:44 +0000

    a

--- trunk/oi_musas_awr/prepare_exec_cmd.sh
+++ trunk/oi_musas_awr/prepare_exec_cmd.sh
@@ -4,4 +4,4 @@
 # Prepare the command line to look like:
 #${HOME}/oi_musas_awr/awr_reports.sh TEST perfstat `date -I` 14:00 15:00
 
-$HOME/scripto/perl/ask_ldap.pl "orainfDbMusasMonitoring=TRUE" "['cn','orainfDbRrdoraUser','orainfDbMusasStart','orainfDbMusasEnd']" | awk '{ print "${HOME}/oi_musas_awr/awr_reports.sh " $1 " " $2 " `date -I` " $3 " " $4 " > /tmp/awr_reports.log" }' 
+$HOME/scripto/perl/ask_ldap.pl "(&(orainfDbMusasMonitoring=TRUE)(orainfDbMusasMonitoringMode=awr))" "['cn','orainfDbRrdoraUser','orainfDbMusasStart','orainfDbMusasEnd']" | awk '{ print "${HOME}/oi_musas_awr/awr_reports.sh " $1 " " $2 " `date -I` " $3 " " $4 " > /tmp/awr_reports.log" }' 
