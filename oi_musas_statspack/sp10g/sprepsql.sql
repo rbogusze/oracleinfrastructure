4758287528aa91ce6aa125c33a128d363311e4a1
Remigiusz.Boguszewicz <Remigiusz.Boguszewicz@1a1a0f9a-0ea6-11df-9e6e-d17b0969bdea>
2013-02-21 13:57:24 +0000

    a

---
+++ oi_musas_statspack/sp10g/sprepsql.sql
@@ -0,0 +1,46 @@
+Rem
+Rem $Header: /CVS/cvsadmin/cvsrepository/admin/projects/musas1x/sp10g/sprepsql.sql,v 1.1 2011/07/27 20:12:52 remikcvs Exp $
+Rem
+Rem sprepsql.sql
+Rem
+Rem Copyright (c) 2000, 2002, Oracle Corporation.  All rights reserved.  
+Rem
+Rem    NAME
+Rem      sprepsql.sql - StatsPack REPort SQL
+Rem
+Rem    DESCRIPTION
+Rem      This script defaults the dbid and instance number to that of the
+Rem      current instance connected-to, then calls sprsqins.sql to produce
+Rem      the standard Statspack SQL report.
+Rem
+Rem    NOTES
+Rem      Usually run as the PERFSTAT user
+Rem
+Rem    MODIFIED   (MM/DD/YY)
+Rem    cdialeri    10/09/02 - Created
+Rem
+
+--
+-- Get the current database/instance information - this will be used 
+-- later in the report along with bid, eid to lookup snapshots
+
+column inst_num  heading "Inst Num"  new_value inst_num  format 99999;
+column inst_name heading "Instance"  new_value inst_name format a12;
+column db_name   heading "DB Name"   new_value db_name   format a12;
+column dbid      heading "DB Id"     new_value dbid      format 9999999999 just c;
+
+prompt
+prompt Current Instance
+prompt ~~~~~~~~~~~~~~~~
+
+select d.dbid            dbid
+     , d.name            db_name
+     , i.instance_number inst_num
+     , i.instance_name   inst_name
+  from v$database d,
+       v$instance i;
+
+@@sprsqins
+
+--
+-- End of file
---
+++ oi_musas_statspack/sp_snap.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#$Id: sp_snap.sh,v 1.2 2011/07/27 13:50:04 remikcvs Exp $
+#
+# Used to create a statspack snap on DB's where pgfDbMusas1xMonitoring=TRUE
+#
+# Example
+# $ ./sp_snap.sh statspack_password
+#
+
+
+# Load usefull functions
+if [ ! -f $HOME/scripto/bash/bash_library.sh ]; then
+  echo "[error] $HOME/scripto/bash/bash_library.sh not found. Exiting. "
+  exit 1
+else
+  . $HOME/scripto/bash/bash_library.sh
+fi
+
+RECIPIENTS='remigiusz_boguszewicz'
+INFO_MODE=DEBUG
+
+SP_PASS=$1
+check_parameter $SP_PASS
+
+
+SQLPLUS=sqlplus
+F_SQLPLUS=`which sqlplus`
+msgd "F_SQLPLUS: $F_SQLPLUS"
+check_file $F_SQLPLUS
+
+msgd "Generate list of DB's that have pgfDbMusas1xMonitoring=TRUE set"
+F_TMP=/tmp/sp_snap_db.tmp
+
+$HOME/scripto/perl/ask_ldap.pl "pgfDbMusas1xMonitoring=TRUE" "['cn']" > $F_TMP
+
+F_SQL_TMP=/tmp/sp_snap_sql.tmp
+msgd "Loop through list and execute snapschot"
+while read LINE
+do
+  msgd "Connecting to $LINE"
+  $SQLPLUS -s perfstat/${SP_PASS}@$LINE <<EOF > $F_SQL_TMP
+show parameter instance_name
+select sysdate from dual;
+execute statspack.snap;
+select sysdate from dual;
+EOF
+  
+  run_command_d "cat $F_SQL_TMP"
+  
+done < $F_TMP
+
+
+
+
+
+
+
