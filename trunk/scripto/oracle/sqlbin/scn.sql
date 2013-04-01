485553357b45494616f3247551a9dc2b3412ef16
Remigiusz.Boguszewicz <Remigiusz.Boguszewicz@1a1a0f9a-0ea6-11df-9e6e-d17b0969bdea>
2013-04-01 12:50:08 +0000

    a

--- trunk/scripto/oracle/sqlbin/scn.sql
+++ trunk/scripto/oracle/sqlbin/scn.sql
@@ -1,3 +1,4 @@
 column CURRENT_SCN format 999999999999999;
 alter session set NLS_DATE_FORMAT = "YYYY/MM/DD HH24:MI:SS";
-select name, current_scn, sysdate from v$database;
+select inc.INCARNATION#, inc.RESETLOGS_TIME, db.name, db.current_scn, sysdate from V$DATABASE_INCARNATION inc, v$database db where inc.STATUS='CURRENT';
+
