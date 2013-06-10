39a65a5259a335610d5349feb9a5debcbef91665
Remigiusz.Boguszewicz <Remigiusz.Boguszewicz@1a1a0f9a-0ea6-11df-9e6e-d17b0969bdea>
2013-06-10 10:28:01 +0000

    a

--- oi_musas_statspack/prepare_exec_cmd_for_day.sh
+++ oi_musas_statspack/prepare_exec_cmd_for_day.sh
@@ -13,17 +13,17 @@ while [ ${CURRENT} -lt ${END} ]
 do
   OLD=${CURRENT}
   CURRENT=`expr ${CURRENT} + ${STEP}`
-  echo ${CURRENT}
+  #echo ${CURRENT}
 
   #have 0 at the beginning, when it is one number
   TMP_CHK=`echo ${CURRENT} | wc -m`
-  echo "TMP_CHK: $TMP_CHK"
+  #echo "TMP_CHK: $TMP_CHK"
   if [ $TMP_CHK -lt 3 ]; then
     CURRENT="0${CURRENT}"
   fi
 
-  echo ${CURRENT}
-  echo $OLD
+  #echo ${CURRENT}
+  #echo $OLD
   
 
   echo "./get_statspack.sh RAC1 perfstat 2013-06-03--$OLD:00 2013-06-03--${CURRENT}:00 > /dev/null 2>&1" 
