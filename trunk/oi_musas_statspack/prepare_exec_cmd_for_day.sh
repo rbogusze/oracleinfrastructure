31ca7ae596441af2875edd61235b39585750c232
Remigiusz.Boguszewicz <Remigiusz.Boguszewicz@1a1a0f9a-0ea6-11df-9e6e-d17b0969bdea>
2013-06-10 10:26:55 +0000

    a

--- trunk/oi_musas_statspack/prepare_exec_cmd_for_day.sh
+++ trunk/oi_musas_statspack/prepare_exec_cmd_for_day.sh
@@ -2,7 +2,29 @@
 #$Id: prepare_exec_cmd.sh 308 2013-02-21 16:24:44Z Remigiusz.Boguszewicz $
 #
 # Prepare the command line to look like:
-#${HOME}/oi_musas_statspack/get_statspack.sh DB10203 perfstat 2013-02-21--14:45 2013-02-21--15:00
+# ./get_statspack.sh RAC1 perfstat 2013-06-03--16:00 2013-06-03--18:00
+# ./get_statspack.sh RAC1 perfstat 2013-06-03--18:00 2013-06-03--20:00
 
+STEP=1   # How much to increase the version retreived
 
-$HOME/scripto/perl/ask_ldap.pl "(&(orainfDbMusasMonitoring=TRUE)(orainfDbMusasMonitoringMode=statspack))" "['cn','orainfDbRrdoraUser','orainfDbMusasStart','orainfDbMusasEnd']" | awk '{ print "${HOME}/oi_musas_statspack/get_statspack.sh " $1 " " $2 " `date -I`--"$3 " `date -I`--" $4 " > /tmp/statspack_report.log" }' 
+CURRENT=00
+END=23
+while [ ${CURRENT} -lt ${END} ]
+do
+  OLD=${CURRENT}
+  CURRENT=`expr ${CURRENT} + ${STEP}`
+  echo ${CURRENT}
+
+  #have 0 at the beginning, when it is one number
+  TMP_CHK=`echo ${CURRENT} | wc -m`
+  echo "TMP_CHK: $TMP_CHK"
+  if [ $TMP_CHK -lt 3 ]; then
+    CURRENT="0${CURRENT}"
+  fi
+
+  echo ${CURRENT}
+  echo $OLD
+  
+
+  echo "./get_statspack.sh RAC1 perfstat 2013-06-03--$OLD:00 2013-06-03--${CURRENT}:00 > /dev/null 2>&1" 
+done
