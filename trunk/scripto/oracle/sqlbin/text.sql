d3e7ccd62732fdabf30a15a362c9903ecd7c87f2
Remigiusz.Boguszewicz <Remigiusz.Boguszewicz@1a1a0f9a-0ea6-11df-9e6e-d17b0969bdea>
2013-11-07 18:03:17 +0000

    a

--- trunk/scripto/oracle/sqlbin/text.sql
+++ trunk/scripto/oracle/sqlbin/text.sql
@@ -1,2 +1,2 @@
 prompt Show sql text based on old hash_value
-select sql_text from v$sqltext where sql_id = (select SQL_ID from v$sql where OLD_HASH_VALUE=&1) order by piece asc;
+select sql_text from v$sqltext where sql_id = (select distinct SQL_ID from v$sql where OLD_HASH_VALUE=&1) order by piece asc;
