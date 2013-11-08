f3bccccb45801fa8d5daae3b4dd9940e35cf0d48
Remigiusz.Boguszewicz <Remigiusz.Boguszewicz@1a1a0f9a-0ea6-11df-9e6e-d17b0969bdea>
2013-11-08 10:32:14 +0000

    a

--- scripto/oracle/sqlbin/binds.sql
+++ scripto/oracle/sqlbin/binds.sql
@@ -1,3 +1,6 @@
 prompt Show bind values based on old sql hash value
-SELECT name,      position ,      datatype_string ,      was_captured,      value_string  FROM   v$sql_bind_capture  WHERE  sql_id = (select SQL_ID from v$sql where OLD_HASH_VALUE=&1)
+set linesize 200
+column NAME format a10
+column value_string format a20
+SELECT name, value_string, position, datatype_string, was_captured FROM   v$sql_bind_capture  WHERE  sql_id = (select distinct SQL_ID from v$sql where OLD_HASH_VALUE=&1)
 /
