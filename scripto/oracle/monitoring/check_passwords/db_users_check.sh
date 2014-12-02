90cc03d5bd3c13a347dbc76d58a23cd3ada59841
Remigiusz.Boguszewicz <Remigiusz.Boguszewicz@gmail.com>
2014-12-02 21:34:12 +0000

    a

--- scripto/oracle/monitoring/check_passwords/db_users_check.sh
+++ scripto/oracle/monitoring/check_passwords/db_users_check.sh
@@ -18,17 +18,20 @@ else
   . ~/scripto/bash/bash_library.sh
 fi
 
-INFO_MODE=DEBUG
-#INFO_MODE=INFO
+#INFO_MODE=DEBUG
+INFO_MODE=INFO
 
 # Sanity checks
 F_DB_USERS_LIST="./db_users_list.txt"
 check_file $F_DB_USERS_LIST
 
+F_TMP="db_users_check.tmp"
+
 V_SQLPLUS=`which sqlplus`
 msgd "V_SQLPLUS: $V_SQLPLUS"
 check_file $V_SQLPLUS
 
+run_command_e "touch $F_TMP" > /dev/null
 
 msgi "Looping through users and trying to log in:"
 while read LINE
@@ -42,9 +45,21 @@ do
   msgd "V_PASSWORD: $V_PASSWORD"
   check_parameter $V_PASSWORD
   
-  
+  echo "select * from dual;" | $V_SQLPLUS -s ${V_USERNAME}/${V_PASSWORD} > $F_TMP
+  run_command_d "cat $F_TMP"
+
+  TPM_CHK=`cat $F_TMP | wc -l`
+  msgd "TPM_CHK: $TPM_CHK"
+
+  if [ $TPM_CHK -gt 5 ]; then
+    msgd "Unable to login to $V_USERNAME using password $V_PASSWORD"
+  else
+    echo "Account $V_USERNAME can be accessed using password: $V_PASSWORD"
+  fi
 
 
 done < $F_DB_USERS_LIST
+
+rm -f $F_TMP
 
 msgi "Done."
