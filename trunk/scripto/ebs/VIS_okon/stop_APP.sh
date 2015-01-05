05b798dd534eb397bf352644a988b74b781a4e94
Remigiusz.Boguszewicz@gmail.com <Remigiusz.Boguszewicz@gmail.com@1a1a0f9a-0ea6-11df-9e6e-d17b0969bdea>
2015-01-05 13:32:42 +0000

    a

---
+++ trunk/scripto/ebs/VIS_okon/stop_APP.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# 11.5 cd $COMMON_TOP/admin/scripts/$CONTEXT_NAME
+# 12.1
+#cd $INST_TOP/admin/scripts
+#./adstpall.sh apps/apps
+# 12.2
+cd $ADMIN_SCRIPTS_HOME
+./adstpall.sh apps/apps
