0a8deb0b6ce517cca0762294b0bbde0017ee62ba
Remigiusz.Boguszewicz@gmail.com <Remigiusz.Boguszewicz@gmail.com@1a1a0f9a-0ea6-11df-9e6e-d17b0969bdea>
2015-03-18 07:44:20 +0000

    a

--- trunk/scripto/ebs/test/invalids/test_invalids.sh
+++ trunk/scripto/ebs/test/invalids/test_invalids.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$Id: _base_script_block.sh,v 1.1 2012-05-07 13:47:27 remik Exp $
 
-LOG_DIR=/var/tmp/test_invalids
+LOG_DIR=/home/oracle/scripto/ebs/test/invalids/logs
 mkdir -p $LOG_DIR
 
 (. ./test_invalids.wrap 2>&1) | tee $LOG_DIR/test_invalids_log_$1.`date '+%Y-%m-%d--%H:%M:%S'`
