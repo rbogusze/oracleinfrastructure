34a844ab1f8928f03bc9b718139810a2f30cdec8
Remigiusz.Boguszewicz@gmail.com <Remigiusz.Boguszewicz@gmail.com@1a1a0f9a-0ea6-11df-9e6e-d17b0969bdea>
2015-07-26 21:08:29 +0000

    a

--- trunk/scripto/linux/backup/rsync_from_kasztelan_to_synology.sh
+++ trunk/scripto/linux/backup/rsync_from_kasztelan_to_synology.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-rsync -a --no-group --no-perms --omit-dir-times --progress /KASZTELAN/zdjecia /SYNOLOGY_NFS/KASZTELAN_rsync
+rsync -a --no-group --no-perms --omit-dir-times --progress --delete /KASZTELAN/zdjecia /SYNOLOGY_NFS/KASZTELAN_rsync > /tmp/rsync_from_kasztelan_to_synology.log
+rsync -a --no-group --no-perms --omit-dir-times --progress --delete /KASZTELAN/mirror /SYNOLOGY_NFS/KASZTELAN_rsync >> /tmp/rsync_from_kasztelan_to_synology.log
+cat /tmp/rsync_from_kasztelan_to_synology.log | mail -s "Rsync-log `date -I`" remigiusz.boguszewicz@gmail.com
