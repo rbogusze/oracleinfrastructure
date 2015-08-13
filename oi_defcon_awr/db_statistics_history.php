4d897cf549d5db87f083bb048cd512a281f19d11
rbogusze <remigiusz.boguszewicz@gmail.com>
2015-08-13 15:07:53 +0200

    minor fixes

--- oi_defcon_awr/db_statistics_history.php
+++ oi_defcon_awr/db_statistics_history.php
@@ -22,7 +22,7 @@ if (is_dir($dir)) {
    if ($dh = opendir($dir)) {
       while (($file = readdir($dh)) !== false) {
         //echo "filename: $file : filetype: " . filetype($dir . $file) . "<BR>";
-        if ( filetype($dir . $file) == file  ) {
+        if ( filetype($dir . $file) == 'file'  ) {
           if ( strstr( $file, ".txt") ) {
             $filenames_array[$filenames_array_counter] = $file;
             $filenames_array_counter++;
@@ -64,13 +64,13 @@ for($i=0; $i<count($filenames_array); $i++)
         //echo "<br> pos: " . strpos( $data, $statname );
         if ( strpos( $data, $statname ) == 0 ) {
           //echo "<br> data: $data <br>";
-          list($trash1, $trash2, $trash3, $trash4, $trash5, $trash6, $trash7) = preg_split("/[\s][\s]+/",$data);
+          list($trash1, $trash2, $trash3, $trash4) = preg_split("/[\s][\s]+/",$data);
           $trash2 = str_replace(",", "", $trash2);  // | Total   |
           $trash3 = str_replace(",", "", $trash3);  // | per Second   |
           $trash4 = str_replace(",", "", $trash4);  // | per Trans   |
 
           if ( is_numeric($trash2) && is_numeric($trash3)  ) {
-            //echo "<br> data: $data <br>";
+            // echo "<br> data: $data <br>";
             //echo "<tr><td><a href=\"db_statistics_history.php?dir="  . $dir . "&statname=" . $trash1 . "\" >" . $trash1 . "</a>" . "</td><td>$trash2</td><td>$trash3</td><td>$trash4</td></tr>";
             $data_values1[$data_values_counter] = $trash2;
             $data_values2[$data_values_counter] = $trash3;
@@ -106,10 +106,14 @@ for($i=0; $i<count($filenames_array); $i++)
 
 echo "<br>";
 
+//echo "<BR>before the draw";
+//show_array($data_values1);
+//echo "<BR>before the draw2";
+//show_array($filenames_array_date);
 
-draw_chart($data_values1, $filenames_array_date, ("Statistik: " . $statname), "Total", 0, "");
-//draw_chart($data_values2, $filenames_array, ("Statistik: " . $statname), "per Second", 0, "");
-//draw_chart($data_values3, $filenames_array, ("Statistik: " . $statname), "per Trans", 0, "");
+draw_chart($data_values1, $filenames_array_date, ("Statistik: " . $statname), "Total", 0, "", "");
+//draw_chart($data_values2, $filenames_array_date, ("Statistik: " . $statname), "per Second", 0, "");
+//draw_chart($data_values3, $filenames_array_date, ("Statistik: " . $statname), "per Trans", 0, "");
 
 
 
