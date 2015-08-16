e3020b3410a80e8244d1c098f4b6cceefc4ddaa2
rbogusze <remigiusz.boguszewicz@gmail.com>
2015-08-16 20:57:58 +0200

    a

---
+++ oi_oralms/report.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script should be run from crontab and regulari report about some DB statistics like redo switches and sessions killed
+
+#INFO_MODE=DEBUG
+
+# Load usefull functions
+if [ ! -f $HOME/scripto/bash/bash_library.sh ]; then
+  echo "[error] $HOME/scripto/bash/bash_library.sh not found. Exiting. "
+  exit 1
+else
+  . $HOME/scripto/bash/bash_library.sh
+fi
+
+
