e0138a1c5272348bfed29c44d04a9486b5b38b43
rbogusze <remigiusz.boguszewicz@gmail.com>
2015-08-16 21:29:28 +0200

    a

--- oi_oralms/report.sh
+++ oi_oralms/report.sh
@@ -11,4 +11,37 @@ else
   . $HOME/scripto/bash/bash_library.sh
 fi
 
+f_report_stats_from_dir()
+{
+  msgd "${FUNCNAME[0]} Begin."
+  TMP_LOG_DIR=$1
+  V_DATE=`date -I`
+  V_HOUR=`date '+%Y-%m-%d--%H'`
+  
+  check_directory $TMP_LOG_DIR
+
+  for FILE in `ls $TMP_LOG_DIR | grep $V_DATE`
+  do
+    msgd "FILE: $FILE"
+    V_COUNT=`cat $TMP_LOG_DIR/$FILE | grep $V_HOUR | wc -l`
+    msgd "V_COUNT: $V_COUNT"
+    V_SHORT_FILE=`echo $FILE | awk -F"_" '{print $1}'`
+    msgd "V_SHORT_FILE: $V_SHORT_FILE"
+    #echo "$V_SHORT_FILE | $V_COUNT" 
+
+    printf "%-10s" "$V_SHORT_FILE"
+    echo " $V_COUNT" 
+#exit 0
+
+  done
+
+
+  msgd "${FUNCNAME[0]} End."
+} #f_report_stats_from_dir
+
+
+# Actual run
+echo "Hourly redo switches"
+f_report_stats_from_dir "/tmp/oralms_redo"
+
 
