075f160fb28471a31abe8248789390efa18cccdc
rbogusze <remigiusz.boguszewicz@gmail.com>
2015-10-02 13:58:07 +0200

    a

--- scripto/ebs/LT_monitoring/run_initial_checks.sh
+++ scripto/ebs/LT_monitoring/run_initial_checks.sh
@@ -28,12 +28,23 @@ f_LT_execute_sql()
     exit 1
   fi
 
+  # file name with instructions
   F_LT=$1
   msgd "F_LT: $F_LT" 
   run_command_d "cat $F_LT"
+
+  # Get the SQL to be executed
   V_SQL=`cat $F_LT | grep ^SQL | sed -e 's/^SQL:\ //'`
   msgd "V_SQL: $V_SQL"
 
+  # Get the execution expected attributes
+  # time elapsed
+  V_ELAPSED=`cat $F_LT | grep ^ELAPSED | sed -e 's/^ELAPSED:\ //'`
+  msgd "V_ELAPSED: $V_ELAPSED"
+  V_GETS=`cat $F_LT | grep ^GETS | sed -e 's/^GETS:\ //'`
+  msgd "V_GETS: $V_GETS"
+  V_RESULT=`cat $F_LT | grep ^RESULT | sed -e 's/^RESULT:\ //'`
+  msgd "V_RESULT: $V_RESULT"
 
 #exit 0
 
@@ -68,18 +79,53 @@ EOF`
 
 
   F_TMP=/tmp/run_initial_checks_execute.txt
-  sqlplus /nolog << EOF > $F_TMP
+  sqlplus -s /nolog << EOF > $F_TMP
+  set head off pagesize 0 echo off verify off feedback off heading off
   connect $V_USER/$V_PASS@$CN
   set timing on
   set autotrace on
   $V_SQL
 EOF
-  
+ 
+  # Saving the received values 
   run_command_d "cat $F_TMP"
   V_LT_RESULT_ELAPSED=`cat $F_TMP | grep "^Elapsed:" | sed -e 's/Elapsed:\ //'`
   msgd "V_LT_RESULT_ELAPSED: $V_LT_RESULT_ELAPSED"
   V_LT_RESULT_GETS=`cat $F_TMP | grep "consistent gets" | awk '{print $1}' `
   msgd "V_LT_RESULT_GETS: $V_LT_RESULT_GETS"
+  V_LT_RESULT=`cat $F_TMP | head -1 | awk '{print $1}' `
+  msgd "V_LT_RESULT: $V_LT_RESULT"
+
+
+
+
+  # Comparing the received values with expected
+  msgd "Time elapsed"
+  V_TMP1=`echo $V_ELAPSED | sed -e 's/://g'`
+  msgd "V_TMP1: $V_TMP1"
+  V_TMP2=`echo $V_LT_RESULT_ELAPSED | awk -F"." '{print $1}' | sed -e 's/://g'`
+  msgd "V_TMP2: $V_TMP2"
+
+  if [ $V_TMP2 -lt $V_TMP1 ]; then
+    msgd "OK, execution time less than expected"
+  else
+    msge "BAD, execution took longer than expected"
+  fi
+
+  msgd "Gets"
+  if [ $V_LT_RESULT_GETS -lt $V_GETS ]; then
+    msgd "OK, nr of gets time less than expected"
+  else
+    msge "BAD, nr of gets larger than expected. Acutal: $V_LT_RESULT_GETS Expected: $V_GETS"
+  fi
+
+  msgd "Result"
+  if [ $V_LT_RESULT -lt $V_RESULT ]; then
+    msgd "OK, result less than expected"
+  else
+    msge "BAD, result larger than expected. Acutal: $V_LT_RESULT Expected: $V_RESULT"
+  fi
+  
 
 
   msgd "${FUNCNAME[0]} End."
