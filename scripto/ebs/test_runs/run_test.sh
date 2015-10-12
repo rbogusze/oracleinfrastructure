c5023894eee00e111dd03b8cf69794d2f716c229
rbogusze <remigiusz.boguszewicz@gmail.com>
2015-10-12 16:23:01 +0200

    a

--- scripto/ebs/test_runs/run_test.sh
+++ scripto/ebs/test_runs/run_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$Id: _base_script_block.sh,v 1.1 2012-05-07 13:47:27 remik Exp $
 
-LOG_DIR=/var/tmp/linux_setup
+LOG_DIR=/var/tmp/run_test
 mkdir -p $LOG_DIR
 
-(. ./linux_setup.wrap 2>&1) | tee $LOG_DIR/linux_setup_log_$1.`date '+%Y-%m-%d--%H:%M:%S'`
+(. ./run_test.wrap 2>&1) | tee $LOG_DIR/run_test_log_$1.`date '+%Y-%m-%d--%H:%M:%S'`
