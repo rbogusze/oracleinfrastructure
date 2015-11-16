fd11807d1e8419a7363426f7cb01e37c24689b02
rbogusze <remigiusz.boguszewicz@gmail.com>
2015-11-16 12:43:29 +0100

    cleanup

--- scripto/ebs/LT_monitoring/run_initial_checks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#$Id: _base_script_block.sh,v 1.1 2012-05-07 13:47:27 remik Exp $
-
-LOG_DIR=/var/tmp/LT_monitoring
-mkdir -p $LOG_DIR
-
-(. ./run_initial_checks.wrap 2>&1) | tee $LOG_DIR/run_initial_checks_log_$1.`date '+%Y-%m-%d--%H:%M:%S'`
