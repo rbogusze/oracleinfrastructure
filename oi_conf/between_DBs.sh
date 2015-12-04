d37610f933917920916648c831adab3a88074e2d
rbogusze <remigiusz.boguszewicz@gmail.com>
2015-12-04 23:14:28 +0100

    a

--- oi_conf/between_DBs.sh
+++ oi_conf/between_DBs.sh
@@ -15,6 +15,18 @@ D_CONF_REPO=$HOME/conf_repo
 D_BETWEEN=$HOME/conf_repo/between_DBs
 F_BETWEEN_INIT=init.ora
 
+msgd "Cleanup for $F_BETWEEN_INIT"
+run_command_e "cd $D_BETWEEN"
+run_command_e "rm -f $F_BETWEEN_INIT"
+cvs remove $F_BETWEEN_INIT
+cvs commit -m "removing $V_CN" $F_BETWEEN_INIT
+msgd "To have fresh numbers and no history I need to delete the file from repository"
+
+
+exit 0
+
+
+
 msgi "Look for init files and copy all of them into one file with versioning"
 for i in `find ${D_CONF_REPO} | grep "init.*.ora" | grep -v "between_DBs"`
 do
@@ -22,6 +34,7 @@ do
   msgd "Figure out the CN from the path"
   V_CN=`echo $i | awk -F'/' '{print $(NF-1)}'`
   msgd "V_CN: $V_CN"
+
 
   msgd "Copy DB init into between init , commit with CN message"  
   run_command_e "cd $D_BETWEEN"
