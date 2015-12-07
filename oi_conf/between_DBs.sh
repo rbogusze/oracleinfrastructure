7f1993a5e51b497f85f9028a0f8f9d208dae2db4
rbogusze <remigiusz.boguszewicz@gmail.com>
2015-12-07 12:18:39 +0100

    a

--- oi_conf/between_DBs.sh
+++ oi_conf/between_DBs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 
 
-INFO_MODE=DEBUG
+#INFO_MODE=DEBUG
 
 # Load usefull functions
 if [ ! -f $HOME/scripto/bash/bash_library.sh ]; then
@@ -13,6 +13,8 @@ fi
 
 D_CONF_REPO=$HOME/conf_repo
 D_BETWEEN=$HOME/conf_repo/between_DBs
+
+#################### init.ora ############################
 F_BETWEEN_INIT=init.ora
 
 msgd "Cleanup for $F_BETWEEN_INIT"
@@ -22,8 +24,6 @@ cvs remove $F_BETWEEN_INIT
 cvs commit -m "removing $V_CN" $F_BETWEEN_INIT
 msgd "To have fresh numbers and no history I need to delete the file from repository"
 run_command "rm -f /home/cvs/conf_repo/between_DBs/Attic/init.ora,v"
-
-
 
 msgi "Look for init files and copy all of them into one file with versioning"
 for i in `find ${D_CONF_REPO} | grep "init.*.ora" | grep -v "between_DBs"`
@@ -41,6 +41,64 @@ do
   cvs add $F_BETWEEN_INIT
   cvs commit -m "from $V_CN" $F_BETWEEN_INIT
 
-#exit 0
+done
+
+
+#################### SPM.txt ############################
+F_BETWEEN_INIT=SPM.txt
+
+msgd "Cleanup for $F_BETWEEN_INIT"
+run_command_e "cd $D_BETWEEN"
+run_command_e "rm -f $F_BETWEEN_INIT"
+cvs remove $F_BETWEEN_INIT
+cvs commit -m "removing $V_CN" $F_BETWEEN_INIT
+msgd "To have fresh numbers and no history I need to delete the file from repository"
+run_command "rm -f /home/cvs/conf_repo/between_DBs/Attic/${F_BETWEEN_INIT},v"
+
+msgi "Look for init files and copy all of them into one file with versioning"
+for i in `find ${D_CONF_REPO} | grep "${F_BETWEEN_INIT}" | grep -v "between_DBs"`
+do
+  echo $i
+  msgd "Figure out the CN from the path"
+  V_CN=`echo $i | awk -F'/' '{print $(NF-1)}'`
+  msgd "V_CN: $V_CN"
+
+
+  msgd "Copy DB init into between init , commit with CN message"  
+  run_command_e "cd $D_BETWEEN"
+  run_command "cat $i | sort > $F_BETWEEN_INIT"
+  cvs add $F_BETWEEN_INIT
+  cvs commit -m "from $V_CN" $F_BETWEEN_INIT
 
 done
+
+
+#################### AD_BUGS.txt ############################
+F_BETWEEN_INIT=AD_BUGS.txt
+
+msgd "Cleanup for $F_BETWEEN_INIT"
+run_command_e "cd $D_BETWEEN"
+run_command_e "rm -f $F_BETWEEN_INIT"
+cvs remove $F_BETWEEN_INIT
+cvs commit -m "removing $V_CN" $F_BETWEEN_INIT
+msgd "To have fresh numbers and no history I need to delete the file from repository"
+run_command "rm -f /home/cvs/conf_repo/between_DBs/Attic/${F_BETWEEN_INIT},v"
+
+msgi "Look for init files and copy all of them into one file with versioning"
+for i in `find ${D_CONF_REPO} | grep "${F_BETWEEN_INIT}" | grep -v "between_DBs"`
+do
+  echo $i
+  msgd "Figure out the CN from the path"
+  V_CN=`echo $i | awk -F'/' '{print $(NF-1)}'`
+  msgd "V_CN: $V_CN"
+
+
+  msgd "Copy DB init into between init , commit with CN message"  
+  run_command_e "cd $D_BETWEEN"
+  run_command "cat $i | sort > $F_BETWEEN_INIT"
+  cvs add $F_BETWEEN_INIT
+  cvs commit -m "from $V_CN" $F_BETWEEN_INIT
+
+done
+
+
