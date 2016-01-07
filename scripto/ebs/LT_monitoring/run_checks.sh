4ea87fb7ad23bda760de067bfcddbe234d8028c5
rbogusze <remigiusz.boguszewicz@gmail.com>
2016-01-07 14:12:28 +0100

    moved pass verification to main section

--- scripto/ebs/LT_monitoring/run_checks.sh
+++ scripto/ebs/LT_monitoring/run_checks.sh
@@ -22,17 +22,6 @@ F_WIKI_SUMMARY=$LOG_DIR/00_wiki_summary.txt
 f_LT_execute_sql()
 {
   msgd "${FUNCNAME[0]} Begin."
-  CN=`cat ~/.LT_SID`  
-  msgd "CN: $CN"
-
-  tnsping ${CN} > /tmp/run_initial_checks_tnsping.txt
-  if [ $? -eq 0 ]; then
-    msgd "OK, tnsping works"
-  else
-    msge "Error, tnsping $CN does not work. Exiting"
-    run_command_d "cat /tmp/run_initial_checks_tnsping.txt"
-    exit 1
-  fi
 
   # file name with instructions
   F_LT=$1
@@ -80,35 +69,6 @@ f_LT_execute_sql()
   msgd "V_RESULT_STR: $V_RESULT_STR"
 
 #exit 0
-
-  V_USER=apps
-  msgd "Getting password from hash"
- 
-  PWD_FILE=~/.passwords
-  INDEX_HASH=`$HOME/scripto/perl/ask_ldap.pl "(cn=$CN)" "['orainfDbRrdoraIndexHash']" 2>/dev/null | grep -v '^ *$' | tr -d '[[:space:]]'`
-  msgd "INDEX_HASH: $INDEX_HASH"
-  HASH=`echo "$INDEX_HASH" | base64 --decode -i`
-  msgd "HASH: $HASH"
-  if [ -f "$PWD_FILE" ]; then
-    V_PASS=`cat $PWD_FILE | grep $HASH | awk '{print $2}' | base64 --decode -i`
-    #msgd "V_PASS: $V_PASS"
-  else
-    msge "Unable to find the password file. Exiting"
-    exit 0
-  fi
-
-  # OK, I have username, password and the database, it is time to connect
-  testavail=`sqlplus -S /nolog <<EOF
-set head off pagesize 0 echo off verify off feedback off heading off
-connect $V_USER/$V_PASS@$CN
-select trim(1) result from dual;
-exit;
-EOF`
-
-  if [ "$testavail" != "1" ]; then
-    msge "DB $CN not available, exiting !!"
-    exit 0
-  fi
 
 
   F_TMP=/tmp/run_initial_checks_execute.txt
@@ -366,7 +326,65 @@ D_INITIAL_CHECKS=$D_BASE/R12_checks
 
 check_directory $D_INITIAL_CHECKS
 CN=`cat ~/.LT_SID`
-#msgi "Running tests on: $CN"
+msgd "Running tests on: $CN"
+
+msgd "Determining $CN availability"
+CN=`cat ~/.LT_SID`  
+msgd "CN: $CN"
+
+tnsping ${CN} > /tmp/run_initial_checks_tnsping.txt
+if [ $? -eq 0 ]; then
+  msgd "OK, tnsping works"
+else
+  msge "Error, tnsping $CN does not work. Exiting"
+  run_command_d "cat /tmp/run_initial_checks_tnsping.txt"
+  exit 1
+fi
+
+msgd "Determining DB username and password"
+V_USER=apps
+msgd "V_USER: $V_USER"
+  msgd "Getting password from hash"
+ 
+  PWD_FILE=~/.passwords
+  INDEX_HASH=`$HOME/scripto/perl/ask_ldap.pl "(cn=$CN)" "['orainfDbRrdoraIndexHash']" 2>/dev/null | grep -v '^ *$' | tr -d '[[:space:]]'`
+  msgd "INDEX_HASH: $INDEX_HASH"
+  if [ -z "$INDEX_HASH" ]; then
+    msgd "INDEX_HASH is empty, what means we do not have password stored in password file"
+    echo -n "[wait] Provide the password for user $V_USER: "
+    read -s V_PASS
+    echo
+    if [ -z "$V_PASS" ]; then 
+      msge "Empty password provided. Exiting."
+      exit 1
+    fi
+  else 
+    HASH=`echo "$INDEX_HASH" | base64 --decode -i`
+    msgd "HASH: $HASH"
+    if [ -f "$PWD_FILE" ]; then
+      V_PASS=`cat $PWD_FILE | grep $HASH | awk '{print $2}' | base64 --decode -i`
+      #msgd "V_PASS: $V_PASS"
+    else
+      msge "Unable to find the password file. Exiting"
+      exit 0
+    fi
+  fi
+
+  msgd "OK, I have username, password and the database, it is time to test the connection"
+  testavail=`sqlplus -S /nolog <<EOF
+set head off pagesize 0 echo off verify off feedback off heading off
+connect $V_USER/$V_PASS@$CN
+select trim(1) result from dual;
+exit;
+EOF`
+
+  if [ "$testavail" != "1" ]; then
+    msge "DB $CN not available, exiting !!"
+    exit 0
+  fi
+
+#WIP
+
 
 echo "====== Checking: $CN on: `date`======" >> $F_WIKI_SUMMARY
 echo '~~NOTOC~~' >> $F_WIKI_SUMMARY
