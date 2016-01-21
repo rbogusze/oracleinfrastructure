526a3220765dacbe5452b7226a818c40323ca803
rbogusze <remigiusz.boguszewicz@gmail.com>
2016-01-21 22:33:28 +0100

    a

--- oi_orlnms/launch_logstalgia.sh
+++ oi_orlnms/launch_logstalgia.sh
@@ -1,1 +1,1 @@
-tail -f /tmp/global_listener_raw.log | grep -v service_update | ./parser.sh | logstalgia -
+tail -f /tmp/global_listener_raw.log | grep -v service_update | ./parser.sh | logstalgia --no-bounce --hide-paddle --sync -
