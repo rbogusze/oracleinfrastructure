1249287f4e3917b3e456fc62c7b9158964972e93
rbogusze <remigiusz.boguszewicz@gmail.com>
2016-02-08 14:08:31 +0100

    messy but looks usefull

--- oi_defcon_awr11/sga_breakdown.php
+++ oi_defcon_awr11/sga_breakdown.php
@@ -16,7 +16,7 @@ echo "<BR> dir: $dir";
 echo "<BR> filename: $filename";
 
 echo "<table border=1>";
-echo "<tr><td>Statistic</td><td>Total</td><td>per Second</td><td>per Trans</td></tr>";
+echo "<tr><td>Pool name</td><td>Begin MB</td><td>End MB</td><td>% Diff</td></tr>";
 //Open the file and read the Statistics section
 if (is_file($dir . $filename) ) {
   $fh = fopen ($dir . $filename, "r") or die("Could not open file");
@@ -27,9 +27,9 @@ if (is_file($dir . $filename) ) {
 //    $data = str_replace("\f",'',$data);
     if (strlen($data) == 0) { continue; }
     // Spot sections start
-    if (strstr ( $data, "Instance Activity Stats ")) { echo "<font color='black'>"; $section = 1; }
+    if (strstr ( $data, "SGA breakdown difference ")) { echo "<font color='black'>"; $section = 1; }
     // Spot section end, then exit the while loop.
-    if (strstr ( $data, "Instance Activity Stats - Absolute Values")) { echo "<font color='red'>"; $section = 0; break; }
+    if (strstr ( $data, "Streams CPU/IO Usage ")) { echo "<font color='red'>"; $section = 0; break; }
     if ( $section ) {
       // I need to stripe multiple spaces
 //      $data = preg_replace('/\s\s+/', ' ', $data);
@@ -38,7 +38,16 @@ if (is_file($dir . $filename) ) {
       // Separate the string into pieces
       //echo "<br> data: $data <br>";
       //list($trash1, $trash2, $trash3, $trash4, $trash5, $trash6, $trash7) = split('  ',$data);
+      // Delete tab
+      //$data = trim(preg_replace('/\t+/', '', $data));
+
+      // split org
       list($trash1, $trash2, $trash3, $trash4, $trash5, $trash6, $trash7) = preg_split("/[\s][\s]+/",$data);
+      // split into text and numbers
+      //preg_match_all('/[^\d]+/', $data, $textMatch);
+      //preg_match('/\d+/', $data, $numMatch);
+      //$trash1 = $textMatch[1];
+
       //echo "<br> trash1: $trash1 | trash2: $trash2 | trash3: $trash3 | trash4: $trash4 <br>";
      
       $trash2 = str_replace(",", "", $trash2);  // | Total   |
@@ -47,7 +56,7 @@ if (is_file($dir . $filename) ) {
 
       if ( is_numeric($trash2) && is_numeric($trash3)  ) {
         //echo "<br> data: $data <br>";
-        echo "<tr><td><a href=\"db_statistics_history.php?dir="  . $dir . "&statname=" . $trash1 . "\" >" . $trash1 . "</a>" . "</td><td>$trash2</td><td>$trash3</td><td>$trash4</td></tr>";
+        echo "<tr><td><a href=\"sga_breakdown_history.php?dir="  . $dir . "&statname=" . $trash1 . "\" >" . $trash1 . "</a>" . "</td><td>$trash2</td><td>$trash3</td><td>$trash4</td></tr>";
       } // if ( is_numeric($trash2)
 
     } // if ( $section )
