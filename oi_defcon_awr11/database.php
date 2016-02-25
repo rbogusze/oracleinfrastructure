2943fba8476a7f0e84b8f5a3fd6bf577b9b7deef
rbogusze <remigiusz.boguszewicz@gmail.com>
2016-02-25 21:48:13 +0100

    before color

--- oi_defcon_awr11/database.php
+++ oi_defcon_awr11/database.php
@@ -81,6 +81,7 @@ echo "<a href=\"$html_filename\" >" . str_replace(".txt",".html",$filename) . "<
 echo "<BR><BR>";
 echo "<a href=\"db_statistics.php?dir=$dir&filename=$filename\" >Database Statistics</a> ";
 echo "<a href=\"sga_breakdown.php?dir=$dir&filename=$filename\" >Shared Pool Statistics</a> ";
+echo "<a href=\"wait_events.php?dir=$dir&filename=$filename\" >Wait Events</a> ";
 
 //Show diagnostics for the current date
 //echo "<BR><BR> Diagnostic scripts ( includes contents of cache ) <BR>";
