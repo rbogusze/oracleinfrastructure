f89058f121c57f25b2414d9b2c915ef56001efad
rbogusze <remigiusz.boguszewicz@gmail.com>
2016-02-26 16:57:18 +0100

    a

--- oi_oralms/create_html_page.sh
+++ oi_oralms/create_html_page.sh
@@ -16,8 +16,29 @@ tail -n 200 /tmp/global_alert.log | tac > /tmp/global_alert.log_for_html
 cat ~/scripto/html/header.txt
 while read LINE
 do
+  # Make green time stamp
+  if [[ "$LINE" =~ "^... ... .. ..:..:.. ... ...." ]] || [[ "$LINE" =~ "^... ... .. ..:..:.. .... ...." ]]; then
+    echo "<font color=\"green\">"
+  fi
+
+  if [[ "$LINE" =~ "ORA-....." ]] || [[ "$LINE" =~ "terminated" ]] || [[ "$LINE" =~ "Corrupt" ]]; then
+    echo "<font color=\"red\">"
+  fi
+
+  if [[ "$LINE" =~ "Completed: ALTER DATABASE OPEN" ]] || [[ "$LINE" =~ "Completed: ALTER DATABASE CLOSE NORMAL" ]] || [[ "$LINE" =~ "Completed: ALTER DATABASE DISMOUNT" ]]; then
+    echo "<font color=\"yellow\">"
+  fi
+
+  if [[ "$LINE" =~ "cannot allocate new log" ]] || [[ "$LINE" =~ "Checkpoint not complete" ]]; then
+    echo "<font color=\"magenta\">"
+  fi
+
+  if [[ "$LINE" =~ "###" ]] || [[ "$LINE" =~ "warning" ]] || [[ "$LINE" =~ "gather_monitor" ]]; then
+    echo "<font color=\"cyan\">"
+  fi
+
   echo "$LINE" 
-  echo "<BR>"
+  echo "</font><BR>"
 done < /tmp/global_alert.log_for_html
 cat ~/scripto/html/footer.txt 
 
