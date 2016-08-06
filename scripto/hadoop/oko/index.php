5e29df96301d084b1afedf90ad5efcd68eeee621
rbogusze <remigiusz.boguszewicz@gmail.com>
2016-08-06 23:14:57 +0200

    a

--- scripto/hadoop/oko/index.php
+++ scripto/hadoop/oko/index.php
@@ -17,23 +17,28 @@ $oczy = array(
      "location" => "pracownia",
      "video_url" => "http://192.168.1.232:8081",
      "hostname" => "192.168.1.232",
-     "rank" => 100
+     "rank" => 400
   ),
   "oko2" => array (
      "description" => "hejo",
      "location" => "nie wiem gdzie",
      "video_url" => "http://192.168.1.233:8081",
      "hostname" => "192.168.1.233",
-     "rank" => 100
+     "rank" => 400
   ),
   "oko3" => array (
      "description" => "hejo",
      "location" => "hej tam",
      "video_url" => "http://192.168.1.234:8081",
      "hostname" => "192.168.1.233",
-     "rank" => 100
+     "rank" => 400
   )
 );
+
+// measure the activity and based on that change the rank value
+$dir = "";
+
+
 
 // loop through the array
 $last = count($oczy) - 1;
@@ -43,12 +48,13 @@ print "<table border=1>";
 print "<tr><td>";
 foreach ($oczy as $i => $row)
 {
-    $isFirst = ($i == 0);
-    $isLast = ($i == $last);
+  $isFirst = ($i == 0);
+  $isLast = ($i == $last);
 
-    echo "<img src=" . $row['video_url'] . "/ border=0 ></a>";
-    echo $i . " " . $row['location'] . "<BR>";
-    print "</td><td>";
+  echo "<img src=" . $row['video_url'] . "/ border=0 width=" . $row['rank'] . "></a>";
+  echo $i . " " . $row['location'] . "<BR>";
+
+  print "</td><td>";
 }
 print "</tr></td>";
 print "</table>";
