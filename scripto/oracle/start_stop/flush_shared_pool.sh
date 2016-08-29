c01579b3f2d30368a9e7b0d29737dd3ad5fab270
rbogusze <remigiusz.boguszewicz@gmail.com>
2016-08-29 15:27:11 +0200

    a

---
+++ scripto/oracle/start_stop/flush_shared_pool.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#checking if ORACLE_HOME and ORACLE_SID are set
+if [ -z "$ORACLE_HOME" ] || [ -z "$ORACLE_SID" ] ; then
+  echo "ORACLE_HOME or ORACLE_SID not set. Please set the DB environment. Exiting"
+  exit 1
+fi
+
+
+
+# Flush shared pool 
+sqlplus / as sysdba << EOF
+show parameter instance_name
+alter session set NLS_DATE_FORMAT = "YYYY/MM/DD HH24:MI:SS";
+select sysdate from dual;
+EXIT;
+EOF
