db98f11b5478578f2b240cf343538047d094a265
rbogusze <remigiusz.boguszewicz@gmail.com>
2016-09-02 12:44:51 +0200

    diag

--- scripto/oracle/start_stop/flush_shared_pool.sh
+++ scripto/oracle/start_stop/flush_shared_pool.sh
@@ -10,10 +10,16 @@ fi
 
 # Flush shared pool 
 sqlplus / as sysdba << EOF
+prompt Gather current shared pool stats
+set linesize 300
+select * from v\$sgainfo;
+select pool, name, round(bytes/(1024 * 1024)) size_mb from v\$sgastat where pool='shared pool' and bytes > (1024 * 1024) order by size_mb desc;
+
 show parameter instance_name
 alter session set NLS_DATE_FORMAT = "YYYY/MM/DD HH24:MI:SS";
 select sysdate from dual;
 prompt Executing alter system flush shared_pool
+set timing on
 alter system flush shared_pool;
 exit;
 EOF
