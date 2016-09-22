51e6fc7aa07b40ac67024860e06ee7c6d5d37881
rbogusze <remigiusz.boguszewicz@gmail.com>
2016-09-22 09:46:31 +0200

    a

---
+++ oi_musas_awr/prepare_exec_cmd_special.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#$Id$
+#
+# Some special cases
+
+# Load usefull functions                                                     
+if [ ! -f $HOME/scripto/bash/bash_library.sh ]; then                         
+  echo "[error] $HOME/scripto/bash/bash_library.sh not found. Exiting. "     
+  exit 1                                                                     
+else                                                                         
+  . $HOME/scripto/bash/bash_library.sh                                       
+fi                                                                           
+
+# Secelted CN for the whole day every hour, like
+# ./awr_reports_special.sh UAT1 system 2016-09-21 11:00 12:00
+# ./awr_reports_special.sh UAT1 system 2016-09-21 12:00 13:00                                                                             
+DATE=2016-09-22
+
+for i in {1..23}
+do
+   NOW_DATE=$(date +%H:%M  -d "$DATE + $((i - 1)) hour")
+   NEXT_DATE=$(date +%H:%M  -d "$DATE + $i hour")
+   echo "./awr_reports_special.sh UAT1 system $DATE $NOW_DATE $NEXT_DATE"
+done
+
