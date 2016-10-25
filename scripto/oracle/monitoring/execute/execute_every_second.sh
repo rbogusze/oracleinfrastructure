ebdcbc83dff636035548770188d55dec66d3f548
rbogusze <remigiusz.boguszewicz@gmail.com>
2016-10-25 10:54:10 +0200

    a

--- scripto/oracle/monitoring/execute/execute_every_second.sh
+++ scripto/oracle/monitoring/execute/execute_every_second.sh
@@ -13,14 +13,25 @@ else
 fi
 RECIPIENTS='remigiusz.boguszewicz@gmail.com'
 
-INFO_MODE=DEBUG
+#INFO_MODE=DEBUG
 
 CN=$1
 V_USER=$2
 V_PASS=$3
 
+DATE_START=`date --date="2016-10-25 04:27:00"`
+NR_DAYS_BACK=1800
+#NR_DAYS_BACK=2
+
 SQL_FILE=./query.sql
-TMP_FILE=/tmp/execute_every_second.tmp
+TMP_FILE_BASE=/tmp/execute_every_second.tmp
+
+MASTER_FILE=/tmp/execute_every_second.tmp.master
+V_RANDOM=$RANDOM
+F_EQUAL=/tmp/execute_every_second.tmp.${V_RANDOM}_equal
+F_DIFF=/tmp/execute_every_second.tmp.${V_RANDOM}_diff
+touch $F_EQUAL
+touch $F_DIFF
 
 check_variable $CN
 check_variable $V_USER
@@ -29,7 +40,7 @@ check_file $SQL_FILE
 
 msgd "CN: $CN"
 msgd "V_USER: $V_USER"
-msgd "V_PASS: $V_PASS"
+#msgd "V_PASS: $V_PASS"
 
 testavail=`sqlplus -S /nolog <<EOF
 set head off pagesize 0 echo off verify off feedback off heading off
@@ -47,8 +58,6 @@ else
 fi
 
 
-DATE_START=`date --date="2016-10-21 03:07:00"`
-NR_DAYS_BACK=10
 
 myvar=0
 while [ $myvar -ne $NR_DAYS_BACK ]
@@ -57,6 +66,7 @@ do
   #CHECK_FOR_DATE=`date '+%Y-%m-%d--%H:%M:%S' -d "$DATE_START $myvar day ago"`
   CHECK_FOR_DATE=`date '+%Y-%m-%d--%H:%M:%S' -d "$DATE_START $myvar seconds ago"`
   msgd "CHECK_FOR_DATE: $CHECK_FOR_DATE"
+  TMP_FILE=${TMP_FILE_BASE}_$CHECK_FOR_DATE
 
   msgd "Executing SQL. Please wait"
   sqlplus -S /nolog << EOF > $TMP_FILE
@@ -65,11 +75,34 @@ connect $V_USER/$V_PASS@$CN
 @$SQL_FILE $CHECK_FOR_DATE
 EOF
 
-  cat $TMP_FILE
+  #cat $TMP_FILE
   myvar=$(( $myvar + 1 ))
+
+  # if master file does not exists I am creating one based on the last results
+  if [ ! -f $MASTER_FILE ]; then
+    msgi "Creating master file based on last results"
+    cp $TMP_FILE $MASTER_FILE
+  fi
+
+  # comparing the results with the master
+  diff $MASTER_FILE $TMP_FILE
+  if [ $? -eq 0 ]; then
+    msgi "No change on $CHECK_FOR_DATE"
+    echo "$CHECK_FOR_DATE" >> $F_EQUAL
+  else
+    msgi "CHANGE!!! on $CHECK_FOR_DATE"
+    echo "$CHECK_FOR_DATE" >> $F_DIFF
+  fi
 
 #exit 0
 done
+
+msgi "Results summary:"
+echo "Equal: $F_EQUAL"
+cat $F_EQUAL | wc -l
+
+echo "Different: $F_DIFF"
+cat $F_DIFF | wc -l
 
 echo "Done."
 
