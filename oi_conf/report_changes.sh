8bf377a42c01d3a9e4837caa15f9a4c9030be3e5
rbogusze <remigiusz.boguszewicz@gmail.com>
2017-01-20 11:00:30 +0100

    a

--- oi_conf/report_changes.sh
+++ oi_conf/report_changes.sh
@@ -15,6 +15,11 @@ fi
 
 CVS_DIR=/home/orainf/conf_repo
 RECIPIENTS=remigiusz.boguszewicz@gmail.com
+D_TMP=/tmp/report_changes
+F_TMP=$D_TMP/files
+
+mkdir -p $D_TMP
+check_directory $D_TMP
 
 V_MODE=$1
 msgd "V_MODE: $V_MODE"
@@ -29,16 +34,34 @@ END_DATE=`date -I`
 
 case $V_MODE in
   "dbinit.txt")
-    echo "Provide diff for $V_MODE"
+    msgi "Provide diff for $V_MODE"
+    msgd "Go through the CVS_DIR and search for dbinit.txt files"
+    find $CVS_DIR -name dbinit.txt > $F_TMP
+    check_file $F_TMP
+    run_command_d "cat $F_TMP"
+    msgd "Loop through provided files list and give cvs diff"
+    while read LINE
+    do
+      msgd "LINE: $LINE"
+      D_CVS_FILE=`dirname $LINE`
+      msgd "D_CVS_FILE: $D_CVS_FILE"
+      run_command "cd $D_CVS_FILE"
+      F_CVS_FILE=`basename $LINE`
+      msgd "F_CVS_FILE: $F_CVS_FILE"
+      cvs diff -a -b -B -D $START_DATE -D $END_DATE $F_CVS_FILE | grep -v "cvs server: Diffing" | grep -v "retrieving revision" | grep -v "^diff -a -b -B" 
+
+#exit 0
+    done < $F_TMP
+
     ;;
   "all")
-    echo "Generating raport from $START_DATE to $END_DATE"
+    msgi "Generating raport from $START_DATE to $END_DATE"
     cd $CVS_DIR
     #cvs diff -a -b -B -D $START_DATE -D $END_DATE | grep -v "cvs server: Diffing" | grep -v "retrieving revision" | grep -v "^diff -a -b -B" | mail -s "Orifm weekly changes report" $RECIPIENTS
     cvs diff -a -b -B -D $START_DATE -D $END_DATE | grep -v "cvs server: Diffing" | grep -v "retrieving revision" | grep -v "^diff -a -b -B" 
     ;;
   *)
-    echo "Unknown mode! Exiting."
+    msge "Unknown mode! Exiting."
     exit 1
     ;;
 esac
