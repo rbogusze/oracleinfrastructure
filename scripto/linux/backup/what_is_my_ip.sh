2295c47b3829eb6a5d989211e288424df39c9b83
rbogusze <remigiusz.boguszewicz@gmail.com>
2017-04-25 21:07:02 +0200

    a

--- scripto/linux/backup/what_is_my_ip.sh
+++ scripto/linux/backup/what_is_my_ip.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-dig +short myip.opendns.com @resolver1.opendns.com | mail -s "myip `date '+%d/%m/%Y_%H:%M:%S'`" remigiusz.boguszewicz@gmail.com
+#dig +short myip.opendns.com @resolver1.opendns.com | mail -s "myip `date '+%d/%m/%Y_%H:%M:%S'`" remigiusz.boguszewicz@gmail.com
+echo -n "`date '+%d/%m/%Y_%H:%M:%S'` " >> ~/osobiste/komputery/my_ip.txt
+dig +short myip.opendns.com @resolver1.opendns.com >> ~/osobiste/komputery/my_ip.txt
