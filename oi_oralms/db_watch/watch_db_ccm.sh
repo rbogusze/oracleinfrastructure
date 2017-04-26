a18976327b810818c05ec00ba03d41b94e8a92a1
rbogusze <remigiusz.boguszewicz@gmail.com>
2017-04-26 13:19:33 +0200

    a

--- oi_oralms/db_watch/watch_db_ccm.sh
+++ oi_oralms/db_watch/watch_db_ccm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$Id: _base_script_block.sh,v 1.1 2012-05-07 13:47:27 remik Exp $
 
-LOG_DIR=/tmp/watch_db_ccm
+LOG_DIR=/var/www/html/watch_db_ccm
 mkdir -p $LOG_DIR
 
 (. ./watch_db_ccm.wrap 2>&1) | tee $LOG_DIR/watch_db_ccm_log_`date '+%Y-%m-%d--%H:%M:%S'`
