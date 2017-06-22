80b7da1a12e52619c6f014dea3b4fc326b7b41e7
rbogusze <remigiusz.boguszewicz@gmail.com>
2017-06-22 15:12:33 +0200

    a

--- scripto/oracle/testing/simple_tests/hard_parsing/stresser.sh
+++ scripto/oracle/testing/simple_tests/hard_parsing/stresser.sh
@@ -2,7 +2,7 @@
 #$Id: o,v 1.1 2012-05-07 13:47:27 remik Exp $
 #
 # Usage:
-# $ ./counter scott tiger DB
+# $ ./counter scott tiger DB /tmp/test_binds.sql
 
 # Load usefull functions
 if [ ! -f $HOME/scripto/bash/bash_library.sh ]; then
@@ -18,6 +18,7 @@ INFO_MODE=INFO
 USER=$1
 PASS=$2
 DB=$3
+SQLFILE=$4
 
 msgd "USER: $USER"
 check_parameter $USER
@@ -25,20 +26,26 @@ msgd "PASS: $PASS"
 check_parameter $PASS
 msgd "DB: $DB"
 check_parameter $DB
+msgd "SQLFILE: $SQLFILE"
+check_parameter $SQLFILE
 
-msgi "ala"
-V_PREVIOUS=0
-while [ 1 ]
-do
-  V_P1=$RANDOM
-  V_P2=$RANDOM
-  STARTTIME=$(date +%s%3N)
-  f_user_execute_sql "SELECT * FROM (select task.task_number tasknumber, task.task_name, task_details taskdetails, task.task_id taskid, task.billable_flag, task.project_id, task.start_date, task.completion_date, task.chargeable_flag, proj.project_number, proj.project_details from pa_online_tasks_v task ,pa_online_projects_v proj where proj.project_id = task.project_id) QRSLT WHERE (TaskId = $V_P1 and project_id = $V_P2) ORDER BY tasknumber;" "$USER/$PASS@$DB"
-  msgd "$V_EXECUTE_SQL" 
-  ENDTIME=$(date +%s%3N)
+SQLPLUS=$ORACLE_HOME/bin/sqlplus
+check_file $SQLPLUS
 
-  V_ELA=`expr ${ENDTIME} - ${STARTTIME}`
-  echo "$V_ELA"
-done
+STARTTIME=$(date +%s%3N)
+echo "Start: `date`"
+
+$SQLPLUS -S "$USER/$PASS@$DB" <<EOF > /dev/null
+set heading off
+set linesize 200
+@$SQLFILE
+EOF
+
+
+ENDTIME=$(date +%s%3N)
+
+V_ELA=`expr ${ENDTIME} - ${STARTTIME}`
+echo "End: `date`"
+echo "$V_ELA"
 
 
