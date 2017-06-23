72b0d62c9ba75d2b0e79c1a8d9de1816e4890551
rbogusze <remigiusz.boguszewicz@gmail.com>
2017-06-23 11:54:50 +0200

    a

--- scripto/oracle/testing/simple_tests/hard_parsing/counter.sh
+++ scripto/oracle/testing/simple_tests/hard_parsing/counter.sh
@@ -28,6 +28,7 @@ check_parameter $DB
 
 msgi "ala"
 V_PREVIOUS=0
+V_PREVIOUS2=0
 while [ 1 ]
 do
   f_user_execute_sql "select name, value from v\$sysstat where name = 'parse count (hard)';" "$USER/$PASS@$DB"
@@ -35,10 +36,19 @@ do
   V_CURRENT=`echo $V_EXECUTE_SQL | awk '{print $NF}'`
   msgd "V_PREVIOUS: $V_PREVIOUS"
   V_DELTA=`expr ${V_CURRENT} - ${V_PREVIOUS} `
-  echo $V_DELTA
+  echo "parse count (hard): $V_DELTA"
+
+
+  f_user_execute_sql "select name, value from v\$sysstat where name = 'execute count';" "$USER/$PASS@$DB"
+  msgd "$V_EXECUTE_SQL"
+  V_CURRENT2=`echo $V_EXECUTE_SQL | awk '{print $NF}'`
+  msgd "V_PREVIOUS: $V_PREVIOUS2"
+  V_DELTA=`expr ${V_CURRENT2} - ${V_PREVIOUS2} `
+  echo "executions: $V_DELTA2"
 
   sleep 5
   V_PREVIOUS=$V_CURRENT
+  V_PREVIOUS2=$V_CURRENT2
 done
 
 
