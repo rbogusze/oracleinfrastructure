92dd02a1e9bbc9ba487e9dc946125b235ae0ca3a
rbogusze <remigiusz.boguszewicz@gmail.com>
2017-07-10 15:22:56 +0200

    a

---
+++ scripto/cassandra/generate_test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#$Id: o,v 1.1 2012-05-07 13:47:27 remik Exp $
+#
+# Usage:
+# $ ./generate_test_data_bind.sh 1000
+
+# Load usefull functions
+if [ ! -f $HOME/scripto/bash/bash_library.sh ]; then
+  echo "[error] $HOME/scripto/bash/bash_library.sh not found. Exiting. "
+  exit 1
+else
+  . $HOME/scripto/bash/bash_library.sh
+fi
+
+INFO_MODE=DEBUG
+INFO_MODE=INFO
+
+OUTFILE=/tmp/test_literals.sql
+
+END=$1
+check_parameter $END
+
+msgi "Generating test data"
+CURRENT=0
+STEP=1
+
+rm -f $OUTFILE
+
+while [ ${CURRENT} -ne ${END} ]
+do
+  V_P1=$RANDOM
+  V_P2=$RANDOM
+  echo "INSERT INTO table1 (id, name, temperature) values ($CURRENT, 'Zosia_${V_P1}', $V_P2);" >> $OUTFILE
+
+  CURRENT=`expr ${CURRENT} + ${STEP}`
+done
+
+msgi "Output file can be found in $OUTFILE"
+run_command_d "cat $OUTFILE"
