c92284c2b3a240ba15e680489f9fd4501f1cf1cb
rbogusze <remigiusz.boguszewicz@gmail.com>
2017-07-12 10:32:53 +0200

    a

--- scripto/cassandra/test_data/constant_inserts.sh
+++ scripto/cassandra/test_data/constant_inserts.sh
@@ -15,10 +15,14 @@ do
   echo "INSERT INTO remik1.table1 (id, name, temperature) values (${CURRENT}, 'manual_ubu1_${V_DATE}', 10);" | cqlsh ubu1
   echo "select * from remik1.table1 where id = ${CURRENT};" | cqlsh ubu1
 
+  CURRENT=`expr ${CURRENT} + ${STEP}`
+  V_DATE=`date '+%Y-%m-%d--%H:%M:%S'`
   echo "Executing on ubu2"
   echo "INSERT INTO remik1.table1 (id, name, temperature) values (${CURRENT}, 'manual_ubu2_${V_DATE}', 10);" | cqlsh ubu2
   echo "select * from remik1.table1 where id = ${CURRENT};" | cqlsh ubu2
 
+  CURRENT=`expr ${CURRENT} + ${STEP}`
+  V_DATE=`date '+%Y-%m-%d--%H:%M:%S'`
   echo "Executing on ubu3"
   echo "INSERT INTO remik1.table1 (id, name, temperature) values (${CURRENT}, 'manual_ubu3_${V_DATE}', 10);" | cqlsh ubu3
   echo "select * from remik1.table1 where id = ${CURRENT};" | cqlsh ubu3
