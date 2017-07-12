3118a70c011ac078ac94ede384564222738b96b3
rbogusze <remigiusz.boguszewicz@gmail.com>
2017-07-12 10:22:52 +0200

    a

---
+++ scripto/cassandra/test_data/constant_inserts.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+STEP=1   # How much to increase the version retreived
+CURRENT=1000000
+END=100000000
+
+
+
+while [ ${CURRENT} -ne ${END} ]
+do
+  CURRENT=`expr ${CURRENT} + ${STEP}`
+  V_DATE=`date '+%Y-%m-%d--%H:%M:%S'`
+  echo ${CURRENT}
+  echo "Executing on ubu1"
+  echo "INSERT INTO remik1.table1 (id, name, temperature) values (${CURRENT}, 'manual_ubu1_${V_DATE}', 10);" | cqlsh ubu1
+  echo "select * from remik1.table1 where id = ${CURRENT};" | cqlsh ubu1
+
+  echo "Executing on ubu2"
+  echo "INSERT INTO remik1.table1 (id, name, temperature) values (${CURRENT}, 'manual_ubu2_${V_DATE}', 10);" | cqlsh ubu2
+  echo "select * from remik1.table1 where id = ${CURRENT};" | cqlsh ubu2
+
+  echo "Executing on ubu3"
+  echo "INSERT INTO remik1.table1 (id, name, temperature) values (${CURRENT}, 'manual_ubu3_${V_DATE}', 10);" | cqlsh ubu3
+  echo "select * from remik1.table1 where id = ${CURRENT};" | cqlsh ubu3
+
+  sleep 1
+done
+
+
+
