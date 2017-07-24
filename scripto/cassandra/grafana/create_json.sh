5b80be12565fc552ced684b6f28e35935638e8af
rbogusze <remigiusz.boguszewicz@gmail.com>
2017-07-24 11:26:42 +0200

    a

---
+++ scripto/cassandra/grafana/create_json.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#$Id: o,v 1.1 2012-05-07 13:47:27 remik Exp $
+#
+# Usage:
+# $ ./generate_test_data_bind.sh 1000
+
+# Load usefull functions
+if [ ! -f $HOME/scripto/bash/bash_library.sh ]; then
+  echo "[error] $HOME/scripto/bash/bash_library.sh not found. Exiting. "
+  exit 1
+else
+  . $HOME/scripto/bash/bash_library.sh
+fi
+
+INFO_MODE=DEBUG
+#INFO_MODE=INFO
+
+D_TEMPLATES=~/scripto/cassandra/grafana/templates
+
+echo "ala ma kota"
+F_OUT=/tmp/dashboard_${RANDOM}.json
+msgi "Output file: $F_OUT"
+
+msgd "Take header"
+F_HEADER=$D_TEMPLATES/header.json
+check_file "$F_HEADER"
+cat $F_HEADER >> $F_OUT
+
+msgd "Iterate through the actual graphs"
+msgd "Get the list of sections"
+for i in `find $D_TEMPLATES | grep -v footer.json | grep -v header.json | grep -v foot | grep -v head | grep json`
+do
+  msgd "$i"
+  msgd "Get the filename"
+  F_SECTION_MAIN=`basename $i`
+  msgd "F_SECTION_MAIN: $F_SECTION_MAIN"
+  check_file "$D_TEMPLATES/$F_SECTION_MAIN"
+  msgd "Setup head and tail files for sections"
+  F_SECTION_HEAD=`echo 01_cpu.json | awk -F"." '{print $1"_head.json"}'`
+  msgd "F_SECTION_HEAD: $F_SECTION_HEAD"
+  check_file "$D_TEMPLATES/$F_SECTION_HEAD"
+  F_SECTION_FOOT=`echo 01_cpu.json | awk -F"." '{print $1"_foot.json"}'`
+  msgd "F_SECTION_FOOT: $F_SECTION_FOOT"
+  check_file "$D_TEMPLATES/$F_SECTION_FOOT"
+
+  msgd "Add head"
+  cat $D_TEMPLATES/$F_SECTION_HEAD >> $F_OUT
+
+  
+
+done
+
+
+
+msgd "Take footer"
+F_FOOTER=$D_TEMPLATES/footer.json
+check_file "$F_FOOTER"
+cat $F_FOOTER >> $F_OUT
+
+
+
+
+
+
