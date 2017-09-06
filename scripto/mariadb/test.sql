bcce8a403cdd859c419045236f313f6e044e93b2
rbogusze <remigiusz.boguszewicz@gmail.com>
2017-09-06 09:01:12 +0200

    a

---
+++ scripto/mariadb/test.sql
@@ -0,0 +1,7 @@
+select sum(temperature) from remik.table1;
+update remik.table1 set temperature=1;
+select sum(temperature) from remik.table1;
+update remik.table1 set temperature=2;
+select sum(temperature) from remik.table1;
+update remik.table1 set temperature=3;
+select sum(temperature) from remik.table1;
