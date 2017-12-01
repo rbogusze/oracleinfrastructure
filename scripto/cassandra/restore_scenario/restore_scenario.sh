dc296aed0f72eee2d74d28e532523f927132d8d7
rbogusze <remigiusz.boguszewicz@gmail.com>
2017-12-01 12:26:52 +0100

    a

--- scripto/cassandra/restore_scenario/restore_scenario.sh
+++ scripto/cassandra/restore_scenario/restore_scenario.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$Id: _base_script_block.sh,v 1.1 2012-05-07 13:47:27 remik Exp $
 
-LOG_DIR=/var/tmp/cassandra-docker-restore
+LOG_DIR=/var/tmp/restore_scenario
 mkdir -p $LOG_DIR
 
-(. ./restore.wrap 2>&1) | tee $LOG_DIR/restore_log_`date '+%Y-%m-%d--%H:%M:%S'`
+(. ./restore_scenario.wrap 2>&1) | tee $LOG_DIR/restore_scenario_log_`date '+%Y-%m-%d--%H:%M:%S'`
