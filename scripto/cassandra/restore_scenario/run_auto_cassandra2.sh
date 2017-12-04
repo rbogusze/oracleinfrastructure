d4ff1835aad16e150b046be43f67b136cc675787
rbogusze <remigiusz.boguszewicz@gmail.com>
2017-12-04 16:13:08 +0100

    a

--- scripto/cassandra/restore_scenario/run_auto_cassandra2.sh
+++ scripto/cassandra/restore_scenario/run_auto_cassandra2.sh
@@ -15,17 +15,20 @@ fi
 INFO_MODE=DEBUG
 INFO_MODE=INFO
 
-V_SLEEP=60
+V_SLEEP=90
 
 run_command_e "cd ~/scripto/docker/test_poc/dockerfile_A"
 run_command_e "docker-compose -f docker_compose.yml down"
+run_command_e "cd ~/scripto/docker/test_poc_2.1/dockerfile_B2"
+run_command_e "docker-compose -f docker_compose.yml down"
 run_command_e "cd /var/lib/container_data/cassandra"
-run_command_e "./clear_node_A.sh"
-run_command_e "cd ~/scripto/docker/test_poc/dockerfile_A"
+run_command_e "./clear_node_B.sh"
+
+run_command_e "cd ~/scripto/docker/test_poc_2.1/dockerfile_B2"
 run_command_e "docker-compose -f docker_compose.yml up -d"
 run_command_e "sleep $V_SLEEP"
 run_command_e "docker-compose -f docker_compose.yml up -d"
 run_command_e "sleep $V_SLEEP"
-run_command_e "docker exec cassandra_node_A1 nodetool status"
+run_command_e "docker exec cassandra_node_B1 nodetool status"
 run_command_e "cd /root/scripto/cassandra/restore_scenario"
-run_command_e "./restore_scenario.sh --docker_list cassandra_node_A1,cassandra_node_A2,cassandra_node_A3 --cqlshrc /root/.cassandra/cqlshrcnode_A1 --create_phase_file 01_create_simple_mv.cql --destroy_phase_file 01_destroy_simple.cql"
+run_command_e "./restore_scenario.sh --docker_list cassandra_node_B1,cassandra_node_B2,cassandra_node_B3 --cqlshrc /root/.cassandra/cqlshrcnode_B1 --create_phase_file 01_create_simple.cql --destroy_phase_file 01_destroy_simple.cql"
