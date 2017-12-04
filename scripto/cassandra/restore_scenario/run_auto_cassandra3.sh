c93bd3a8a3d51a29c3c1383c131b584b75963b6d
rbogusze <remigiusz.boguszewicz@gmail.com>
2017-12-04 11:48:48 +0100

    a

--- scripto/cassandra/restore_scenario/run_auto_cassandra3.sh
+++ scripto/cassandra/restore_scenario/run_auto_cassandra3.sh
@@ -15,15 +15,17 @@ fi
 INFO_MODE=DEBUG
 #INFO_MODE=INFO
 
+V_SLEEP=30
+
 run_command_e "cd ~/scripto/docker/test_poc/dockerfile_A"
 run_command_e "docker-compose -f docker_compose.yml down"
 run_command_e "cd /var/lib/container_data/cassandra"
 run_command_e "./clear_node_A.sh"
 run_command_e "cd ~/scripto/docker/test_poc/dockerfile_A"
 run_command_e "docker-compose -f docker_compose.yml up -d"
-run_command_e "sleep 60"
+run_command_e "sleep $V_SLEEP"
 run_command_e "docker-compose -f docker_compose.yml up -d"
-run_command_e "sleep 60"
+run_command_e "sleep $V_SLEEP"
 run_command_e "docker exec cassandra_node_A1 nodetool status"
 run_command_e "cd /root/scripto/cassandra/restore_scenario"
 run_command_e "./restore_scenario.sh --docker_list cassandra_node_A1,cassandra_node_A2,cassandra_node_A3 --cqlshrc /root/.cassandra/cqlshrcnode_A1 --create_phase_file 01_create_simple.cql --destroy_phase_file 01_destroy_simple.cql"
