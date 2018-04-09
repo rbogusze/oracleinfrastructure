308a0d226f607a89e63bf1d1b56d77433e3fc3d5
rbogusze <remigiusz.boguszewicz@gmail.com>
2018-04-09 12:09:11 +0200

    a

--- oi_musas_awr_lite/01_bulk_generate.sh
+++ oi_musas_awr_lite/01_bulk_generate.sh
@@ -44,6 +44,17 @@ error_log()
   done
 }
 
+msgd()
+{
+  if [ "$INFO_MODE" = "DEBUG" ] ; then
+    echo -n "| `/bin/date '+%Y%m%d %H:%M:%S'` "
+    if [ "$V_INTERACTIVE" -eq 1 ]; then echo -e -n '\E[32m'; fi
+    echo -n "[debug]    "
+    if [ "$V_INTERACTIVE" -eq 1 ]; then echo -e -n '\E[39m\E[49m'; fi
+    echo "$1"
+  fi
+}
+
 msgi()
 {
   if [ "$INFO_MODE" = "INFO" ] || [ "$INFO_MODE" = "DEBUG" ] ; then
@@ -55,7 +66,45 @@ msgi()
   fi
 }
 
+check_file()
+{
+  if [ -z "$1" ]; then
+    error_log "[ check_file ] Provided parameter is empty. Exiting. " ${RECIPIENTS}
+    exit 1
+  fi
 
+  if [ ! -f "$1" ]; then
+    error_log "[ error ] $1 not found. Exiting. " ${RECIPIENTS}
+    exit 1
+  else
+    msgd "$1 found."
+  fi
+}
+
+# Runs the command provided as parameter.
+# Does exit if the command fails.
+# Sends error message if the command fails.
+run_command_e()
+{
+  if [ "$INFO_MODE" = "INFO" ] || [ "$INFO_MODE" = "DEBUG" ] ; then
+    msg "\"$1\""
+  fi
+
+  # Determining if we are running in a debug mode. If so wait for any key before eval
+  if [ -n "$DEBUG_MODE" ]; then
+    if [ "$DEBUG_MODE" -eq "1" ] ; then
+      echo "[debug wait] Press any key if ready to run the printed command"
+      read
+    fi
+  fi
+
+  eval $1
+  if [ $? -ne 0 ]; then
+    error_log "[critical] An error occured during: \"$1\". Exiting NOW." ${RECIPIENTS}
+    exit 1
+  fi
+  return 0
+} #run_command_e
 
 
 INFO_MODE=DEBUG
@@ -67,12 +116,14 @@ check_parameter $3
 check_parameter $4
 check_parameter $5
 
-DB_CN=$1
+F_SNAP_FILE=$1
 USERNAME=$2
 TIME_START=$3
 TIME_END=$4
 NR_DAYS_BACK=$5
 DATE_START=$6
+
+check_file $F_SNAP_FILE
 
 myvar=0
 while [ $myvar -ne $NR_DAYS_BACK ]
@@ -88,7 +139,13 @@ do
   if [ "$DAY_OF_WEEK" == "7" ] || [ "$DAY_OF_WEEK" == "6" ]; then
     echo "This is Sunday or Saturday, skiping statspack report generation"
   else
-    echo "./awr_reports.sh ${DB_CN} ${USERNAME} ${CHECK_FOR_DATE} ${TIME_START} ${TIME_END}"
+    echo "Checking file with snaps for date and time"
+    cat $F_SNAP_FILE | grep ${CHECK_FOR_DATE}
+
+exit 0
+
+
+    echo "./awr_reports.sh ${CHECK_FOR_DATE} ${TIME_START} ${TIME_END}"
   fi #if [ "$DAY_OF_WEEK" == "0" ];
 
   myvar=$(( $myvar + 1 ))
