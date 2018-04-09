ebe998345979a5cd8afc18c9e9d433ddb788eaf1
rbogusze <remigiusz.boguszewicz@gmail.com>
2018-04-09 11:52:38 +0200

    a

---
+++ oi_musas_awr_lite/01_bulk_generate.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#$Id$
+#
+# Generate the statspack and hash reports for the long range of days
+#
+# Example
+# $ ./bulk_generate.sh EBSDB4 apps 08:00 16:00 8
+# Optionaly add at the end date from which to start
+# $ ./bulk_generate.sh EBSDB4 apps 08:00 16:00 8 2015-08-12
+#
+#
+# Load usefull functions
+if [ ! -f $HOME/scripto/bash/bash_library.sh ]; then
+  echo "[error] $HOME/scripto/bash/bash_library.sh not found. Exiting. "
+  exit 1
+else
+  . $HOME/scripto/bash/bash_library.sh
+fi
+
+INFO_MODE=DEBUG
+
+# Sanity checks
+check_parameter $1
+check_parameter $2
+check_parameter $3
+check_parameter $4
+check_parameter $5
+
+DB_CN=$1
+USERNAME=$2
+TIME_START=$3
+TIME_END=$4
+NR_DAYS_BACK=$5
+DATE_START=$6
+
+myvar=0
+while [ $myvar -ne $NR_DAYS_BACK ]
+do
+
+  CHECK_FOR_DATE=`date -I -d "$DATE_START $myvar day ago"`
+  msgi "#################################################################"
+  msgi "Computing fo date: ${CHECK_FOR_DATE}"
+  msgi "#################################################################"
+
+  # If this is Sunday or Saturday skip that day
+  DAY_OF_WEEK=`date --date=$CHECK_FOR_DATE '+%u'`
+  if [ "$DAY_OF_WEEK" == "7" ] || [ "$DAY_OF_WEEK" == "6" ]; then
+    echo "This is Sunday or Saturday, skiping statspack report generation"
+  else
+    run_command "./awr_reports.sh ${DB_CN} ${USERNAME} ${CHECK_FOR_DATE} ${TIME_START} ${TIME_END}"
+  fi #if [ "$DAY_OF_WEEK" == "0" ];
+
+  myvar=$(( $myvar + 1 ))
+done
+
+echo "Done."
+
