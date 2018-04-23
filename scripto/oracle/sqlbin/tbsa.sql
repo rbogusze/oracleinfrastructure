26a0b390a8a32711f9cc8c8a99f1c7eded71bb37
rbogusze <remigiusz.boguszewicz@gmail.com>
2018-04-23 09:05:22 +0200

    prevent division by 0

--- scripto/oracle/sqlbin/tbsa.sql
+++ scripto/oracle/sqlbin/tbsa.sql
@@ -1,3 +1,3 @@
 prompt Show tablespace fill ratio that takes under account the autoextend
 prompt That is what Nagios is monitoring and it may be that we have a lot of free space in tablespaces but still we see the warning
-select tablespace_name, round(sum(blocks*8192/1024/1024)) UsedMB, round(sum(maxblocks*8192/1024/1024)) MaxMB, round(sum(blocks)*100/sum(maxblocks)) rate from dba_data_files group by tablespace_name order by rate;
+select tablespace_name, round(sum(blocks*8192/1024/1024)) UsedMB, round(sum(maxblocks*8192/1024/1024)) MaxMB, round(sum(blocks)*100/sum(maxblocks)) rate from dba_data_files where maxblocks > 0 group by tablespace_name order by rate;
