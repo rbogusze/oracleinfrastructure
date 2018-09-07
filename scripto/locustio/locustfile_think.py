c1e777eee8335011d5504a0b3239b02d1be893bc
rbogusze <remigiusz.boguszewicz@gmail.com>
2018-09-07 11:47:43 +0200

    a

---
+++ scripto/locustio/locustfile_think.py
@@ -0,0 +1,25 @@
+from locust import HttpLocust, TaskSet
+
+def ok(l):
+    l.client.get("/ok")
+
+def error(l):
+    l.client.get("/error")
+
+def api(l):
+    l.client.get("/api")
+
+def apis(l):
+    l.client.get("/api?API_SECRET=alamakota")
+
+def heavy(l):
+    l.client.get("/api/heavy")
+
+
+class UserBehavior(TaskSet):
+    tasks = {ok: 3, error: 1, api: 1, apis: 1, heavy: 2}
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 1000
+    max_wait = 3000
