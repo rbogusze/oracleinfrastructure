ea48d1a5b8a054c60f79b36d1bae36da4cd8ee89
rbogusze <remigiusz.boguszewicz@gmail.com>
2018-10-03 11:18:23 +0200

    a

--- scripto/node/app.js
+++ scripto/node/app.js
@@ -10,6 +10,9 @@ http.createServer(function (request, response) {
     console.log('New connection');
     userCount++;
     response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Green\n');
+    response.write('Green\n');
+    response.write('Green\n');
     response.write('We have had '+userCount+' visits!\n');
     response.end('Hello Node JS World\n');
 }).listen(8080);
