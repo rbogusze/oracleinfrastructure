f0a3d5d145bcb22746902a78e79fce94e3940781
rbogusze <remigiusz.boguszewicz@gmail.com>
2019-04-12 10:57:55 +0200

    a

--- scripto/docker/node/app.js
+++ scripto/docker/node/app.js
@@ -6,15 +6,17 @@ sayHello('Remi');
 
 var http = require('http');
 var userCount = 0;
+var os = require("os");
+var hostname = os.hostname();
 http.createServer(function (request, response) {
     console.log('New connection');
     userCount++;
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('<font color="red">This is some text!</font>\n');
-    response.write('Red\n');
-    response.write('Red\n');
+    response.write('<html><head><body>\n');
+    response.write('<font color="red">RED</font><BR>');
+    response.write('I am running on: '+hostname+'<BR>');
     response.write('We have had '+userCount+' visits!\n');
-    response.end('Hello Node JS World\n');
+    response.write('</body></html></head>\n');
+    response.end();
 }).listen(8080);
 
 console.log('Server started');
