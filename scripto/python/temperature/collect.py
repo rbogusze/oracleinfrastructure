352e1f76ccb279dd8f5b0bb47a2e75046135bf4f
rbogusze <remigiusz.boguszewicz@gmail.com>
2019-05-07 21:43:43 +0200

    kafka publish

--- scripto/python/temperature/collect.py
+++ scripto/python/temperature/collect.py
@@ -1,13 +1,13 @@
 # coding: utf8
 from cassandra.cluster import Cluster
 from cassandra.policies import ConstantReconnectionPolicy, DCAwareRoundRobinPolicy
+from json import dumps
+from kafka import KafkaProducer
 import time
 import io
 import socket
 
 # $ sudo pip install python-dateutil
-
-# main endless loop
 
 cluster = Cluster(contact_points=['192.168.1.233','192.168.1.77','192.168.1.27'], idle_heartbeat_interval=5,load_balancing_policy=DCAwareRoundRobinPolicy(), reconnection_policy=ConstantReconnectionPolicy(delay=5, max_attempts=50), idle_heartbeat_timeout=5)
 cluster.connect_timeout = 30
@@ -15,10 +15,17 @@ session = cluster.connect('temperature')
 
 sleep_time = 1 #in seconds
 
+# pip install kafka-python
+producer = KafkaProducer(bootstrap_servers=['sensu:9092'],
+                         value_serializer=lambda x: 
+                         dumps(x).encode('utf-8'))
+
+
 # test
 
 # /test
 
+# main endless loop
 while True:
     
     now = int(time.time())  # time since epoch, in seconds
@@ -39,6 +46,10 @@ while True:
                   + ");"
     #print "cass_insert: %s" % cass_insert
     cass_return = session.execute(cass_insert)
+
+    # Now kafka publish
+    data = {'reading_location' : location, 'reading_date' : str(now), 'reading_value' : str(temp_cpu)}
+    producer.send('temperature', value=data)
                     
     time.sleep(sleep_time)
 
