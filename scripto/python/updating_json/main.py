989282abbf1cc257cda8f3f4124e286aa8584a49
rbogusze <remigiusz.boguszewicz@gmail.com>
2019-06-26 11:39:59 +0200

    a

--- scripto/python/updating_json/main.py
+++ scripto/python/updating_json/main.py
@@ -4,57 +4,52 @@ import time
 import io
 import socket
 import thread
+import random
 
 # $ sudo pip install python-dateutil
 
 sleep_time = 1 #in seconds
 
 # Define a function for the thread
-def print_time( threadName, delay):
-   count = 0
-   while count < 5:
-      time.sleep(delay)
-      count += 1
-      print "%s: %s" % ( threadName, time.ctime(time.time()) )
+
+def update_string (dictionary_id):
+    print "Hello, will update %s" % dictionary_id
+    print "Before change: %s" % strings_dict[dictionary_id]
+    strings_dict[dictionary_id] = "0000changed_that0000"
+    print "After change: %s" % strings_dict[dictionary_id]
+
+
+def trigger_random_update ():
+    random_seed = random.randint(0, 9)
+    print "String random: %s" % random_seed
+    update_string(random_seed)
 
 
 # main 
 
 print "Ala ma kota"
 
-string0 = "000000000000000000"
-string1 = "111111111111111111"
-string2 = "222222222222222222"
-string3 = "333333333333333333"
-string4 = "444444444444444444"
-string5 = "555555555555555555"
-string6 = "666666666666666666"
-string7 = "777777777777777777"
-string8 = "888888888888888888"
-string9 = "999999999999999999"
+strings_dict = {
+  0: "000000000000000000",
+  1: "111111111111111111",
+  2: "222222222222222222",
+  3: "333333333333333333",
+  4: "444444444444444444",
+  5: "555555555555555555",
+  6: "666666666666666666",
+  7: "777777777777777777",
+  8: "888888888888888888",
+  9: "999999999999999999"
+}
 
 while True:
-    random_seed = 2
-    string_name = "string"
-    string_random = string_name + str(random_seed)
 
-    print "String random: %s" % string_random    
-
-    print "Now: %s" % globals()[string_random]
+    trigger_random_update()
+ 
+    print "Sleeping"
     time.sleep(1)
 
 
-
-
-# Create two threads as follows
-try:
-   thread.start_new_thread( print_time, ("Thread-1", 1, ) )
-   thread.start_new_thread( print_time, ("Thread-2", 1, ) )
-except:
-   print "Error: unable to start thread"
-
-while 1:
-   pass
 
 
 
