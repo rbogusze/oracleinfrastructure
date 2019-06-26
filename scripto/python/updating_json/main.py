9efb12bd98153f66477f9bdbc2110fa2bf71f1ff
rbogusze <remigiusz.boguszewicz@gmail.com>
2019-06-26 16:45:57 +0200

    a

--- scripto/python/updating_json/main.py
+++ scripto/python/updating_json/main.py
@@ -4,28 +4,58 @@ import io
 import multiprocessing
 from random import randint
 import logging
+import mysql.connector
 
 start_time = time.time()
 
-#logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-logging.basicConfig(level=logging.WARN, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+#logging.basicConfig(level=logging.WARN, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.debug('This is a log message.')
 
+backend_mysql = True
 
 sleep_time = 1 #in seconds
-iterations = 100
+iterations = 2
+
+cnx = mysql.connector.connect(
+  host="localhost",
+  user="remik",
+  passwd="remik",
+  database="remik"
+)
+
+cursor = cnx.cursor()
+
 
 # Define a function for the thread
 
 def update_string (dictionary_id, worker_num):
     logging.debug("[%s] Before change: %s" % (worker_num, strings_dict[dictionary_id]))
+
+    # Get
     tmp_str = strings_dict[dictionary_id]
-    # replace random character
+
+    if backend_mysql:
+       logging.debug("Get from mysql")
+       query = ("SELECT stringi_text FROM remik.stringi where stringi_id = %s" % dictionary_id)
+
+       cursor.execute(query)
+       
+       for stringi_text in cursor:
+         print "From DB: %s" % stringi_text
+       tmp_str = str(stringi_text)
+    
+    # Prepare random update 
     random_index = randint(0, 19)
     random_seed = randint(0, 9)
     tmp_bstr = bytearray(tmp_str)
     logging.debug("[%s] Hello, will update dict %s index %s to value %s" % (worker_num, dictionary_id, random_index, random_seed))
     tmp_bstr[random_index] = str(random_seed)
+
+    # Update
+    if backend_mysql:
+       logging.debug("Update to mysql")
+
     strings_dict[dictionary_id] = str(tmp_bstr)
 
     logging.debug("[%s] After change : %s" % (worker_num, strings_dict[dictionary_id]))
@@ -58,8 +88,14 @@ strings_dict = {
 }
 
 
-PROCESSES = 5
-WORKER_CALLS = 7
+trigger_random_update(999)
+
+print "Sleep baby"
+time.sleep(1000)
+
+
+PROCESSES = 10
+WORKER_CALLS = 10
 
 def worker(num):
     """worker function"""
