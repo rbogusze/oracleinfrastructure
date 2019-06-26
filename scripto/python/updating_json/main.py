5b522bbf0fa20be64592b4fded014c784ef5accf
rbogusze <remigiusz.boguszewicz@gmail.com>
2019-06-26 15:00:22 +0200

    a

--- scripto/python/updating_json/main.py
+++ scripto/python/updating_json/main.py
@@ -7,20 +7,31 @@ from random import randint
 # $ sudo pip install python-dateutil
 
 sleep_time = 1 #in seconds
+iterations = 100
 
 # Define a function for the thread
 
-def update_string (dictionary_id):
-    print "Hello, will update %s" % dictionary_id
-    print "Before change: %s" % strings_dict[dictionary_id]
-    strings_dict[dictionary_id] = "0000changed_that0000"
-    print "After change: %s" % strings_dict[dictionary_id]
-
-
-def trigger_random_update ():
+def update_string (dictionary_id, worker_num):
+    print "[%s] Before change: %s" % (worker_num, strings_dict[dictionary_id])
+    tmp_str = strings_dict[dictionary_id]
+    # replace random character
+    random_index = randint(0, 19)
     random_seed = randint(0, 9)
-    print "String random: %s" % random_seed
-    update_string(random_seed)
+    tmp_bstr = bytearray(tmp_str)
+    print "[%s] Hello, will update dict %s index %s to value %s" % (worker_num, dictionary_id, random_index, random_seed)
+    tmp_bstr[random_index] = str(random_seed)
+    strings_dict[dictionary_id] = str(tmp_bstr)
+
+    print "[%s] After change : %s" % (worker_num, strings_dict[dictionary_id])
+
+
+def trigger_random_update (worker_num):
+    i = 1
+    while i < iterations:
+      random_seed = randint(0, 9)
+      print "String random: %s" % random_seed
+      update_string(random_seed, worker_num)
+      i += 1
 
 
 # main 
@@ -28,16 +39,16 @@ def trigger_random_update ():
 print "Ala ma kota"
 
 strings_dict = {
-  0: "000000000000000000",
-  1: "111111111111111111",
-  2: "222222222222222222",
-  3: "333333333333333333",
-  4: "444444444444444444",
-  5: "555555555555555555",
-  6: "666666666666666666",
-  7: "777777777777777777",
-  8: "888888888888888888",
-  9: "999999999999999999"
+  0: "00000000000000000000",
+  1: "11111111111111111111",
+  2: "22222222222222222222",
+  3: "33333333333333333333",
+  4: "44444444444444444444",
+  5: "55555555555555555555",
+  6: "66666666666666666666",
+  7: "77777777777777777777",
+  8: "88888888888888888888",
+  9: "99999999999999999999"
 }
 
 
@@ -47,10 +58,12 @@ WORKER_CALLS = 7
 def worker(num):
     """worker function"""
     print 'Starting worker', num
-    trigger_random_update()
+    trigger_random_update(num)
     #time.sleep(randint(2,4))
-    print 'Exiting worker', num
+    print '[%s] Exiting worker' % num
     return "ok"
+
+print strings_dict
 
 if __name__ == '__main__':
     pool = multiprocessing.Pool(processes=PROCESSES)
@@ -58,6 +71,7 @@ if __name__ == '__main__':
     pool.close()
     pool.join()
     print 'Pool:', pool_outputs
+    print strings_dict
 
 
 
