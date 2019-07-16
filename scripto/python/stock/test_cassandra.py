2bf9ce00a5ae68e1608ea159f2216b2aed305385
Remigiusz Boguszewicz <remigiusz.boguszewicz@gmail.com>
2019-07-16 19:43:47 +0200

    a

--- scripto/python/stock/test_cassandra.py
+++ scripto/python/stock/test_cassandra.py
@@ -1,14 +1,11 @@
 # coding: utf8
 from cassandra.cluster import Cluster
 
-cluster = Cluster(['192.168.1.233','192.168.1.236','192.168.1.27'])
-#cluster = Cluster(['192.168.1.233'])
+cluster = Cluster(['192.168.1.20'])
 
 session = cluster.connect('stock')
 
 rows = session.execute('SELECT * FROM transactions')
 for user_row in rows:
     print user_row.asset, user_row.tran_date, user_row.amount
-
-print "ala"
 
