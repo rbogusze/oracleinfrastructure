c000d900d368bab4fcdfb85ef62d9b15ffc50118
Remigiusz Boguszewicz <remigiusz.boguszewicz@gmail.com>
2019-08-11 18:30:53 +0200

    a

--- scripto/python/temperature/multi_collect.sh
+++ scripto/python/temperature/multi_collect.sh
@@ -11,7 +11,7 @@ V_EXECUTE_COUNT=0
 while [ ${V_EXECUTE_COUNT} -lt ${V_EXECUTE} ]
 do
   echo "running $V_EXECUTE_COUNT" 
-  python collect.py &
+  python collect.py $1 $2 $3 $4 &
   V_EXECUTE_COUNT=`expr $V_EXECUTE_COUNT + 1`
 done
 
