7d82b13b026c10a53573873db11b8faa12ac3cc2
Remigiusz Boguszewicz <remigiusz.boguszewicz@gmail.com>
2019-09-05 20:22:55 +0200

    a

---
+++ scripto/k8s/devops_test2/terraform/main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  project = "remi-test-241607"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "default"
+    access_config {
+    }
+  }
+}
