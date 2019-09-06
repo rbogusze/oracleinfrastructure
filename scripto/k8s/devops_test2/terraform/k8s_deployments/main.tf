0cd8cdca769161935a61f67629fdefab811200e6
Remigiusz Boguszewicz <remigiusz.boguszewicz@gmail.com>
2019-09-06 09:02:30 +0200

    a

---
+++ scripto/k8s/devops_test2/terraform/k8s_deployments/main.tf
@@ -0,0 +1,19 @@
+resource "kubernetes_pod" "nginx" {
+  metadata {
+    name = "nginx-example"
+    labels = {
+      App = "nginx"
+    }
+  }
+
+  spec {
+    container {
+      image = "nginx:1.7.8"
+      name  = "example"
+
+      port {
+        container_port = 80
+      }
+    }
+  }
+}
