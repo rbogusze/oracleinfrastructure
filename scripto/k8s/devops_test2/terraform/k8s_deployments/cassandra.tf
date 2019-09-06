7d3ee1564f1f4d4e9781bf68b4603da7ad567e47
Remigiusz Boguszewicz <remigiusz.boguszewicz@gmail.com>
2019-09-06 13:23:46 +0200

    a

--- scripto/k8s/devops_test2/terraform/k8s_deployments/cassandra.tf
+++ scripto/k8s/devops_test2/terraform/k8s_deployments/cassandra.tf
@@ -41,10 +41,23 @@ resource "kubernetes_deployment" "cassandra" {
         container {
           image = "cassandra:3.11.4"
           name  = "cassandra"
-
-
-
+          image_pull_policy = "IfNotPresent"
+          port {
+            container_port = 9042
+          }
+          volume_mount {
+            mount_path = "/var/lib/cassandra"
+            name = "pd-cassandra-volume"
+          }
         }
+
+        volume {
+          name = "pd-cassandra-volume"
+          persistent_volume_claim {
+            claim_name = "pd-cassandra-volume-claim"
+          }
+        }
+
       }
     }
   }
