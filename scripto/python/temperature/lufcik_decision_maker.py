b9ee2527c48565dc645dd42892261936708f1dfb
Remigiusz Boguszewicz <remigiusz.boguszewicz@gmail.com>
2020-05-21 22:59:30 +0200

    a

--- scripto/python/temperature/lufcik_decision_maker.py
+++ scripto/python/temperature/lufcik_decision_maker.py
@@ -3,16 +3,48 @@
 from time import sleep
 from json import loads
 from kafka import KafkaConsumer
+from ConfigParser import SafeConfigParser
 
-consumer = KafkaConsumer('lufcik',
+print("Reading configuration file")
+parser = SafeConfigParser()
+parser.read('/etc/lufcik.ini')
+
+kafka_topic = parser.get('main', 'kafka_topic')
+open_max = parser.get('main', 'open_max')
+
+print("Read from config file \n\t kafka_topic: {} \n\t open_max: {}").format(kafka_topic, open_max)
+
+consumer = KafkaConsumer(kafka_topic,
                          bootstrap_servers=['sensu:9092'],
                          auto_offset_reset='latest',
                          enable_auto_commit=True,
                          group_id='lufcik_decision_maker',
                          )
 
-print("Listening to Kafka topic 'lufcik'")
+def read_open_level():
+    return a
+
+def set_open_level():
+    return a
+
+
+
+
+def f_open():
+    print("opening")
+
+def f_close():
+    print("Closing")
+
+print("Will read from kafka topic: {}").format(kafka_topic)
 for message in consumer:
     message = message.value
     print('Message received: {}'.format(message))
+
+    if message == "open":
+        f_open()
+
+    if message == "close":
+        f_close()
+
     sleep(1)
