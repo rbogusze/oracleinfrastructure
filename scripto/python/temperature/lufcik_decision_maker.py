85659eb9122aca6c3a59cb12904e4cb54301c206
Remigiusz Boguszewicz <remigiusz.boguszewicz@gmail.com>
2020-05-24 22:25:48 +0200

    a

--- scripto/python/temperature/lufcik_decision_maker.py
+++ scripto/python/temperature/lufcik_decision_maker.py
@@ -13,7 +13,7 @@ parser = SafeConfigParser()
 parser.read(parser_file)
 
 kafka_topic = parser.get('main', 'kafka_topic')
-open_max = parser.get('main', 'open_max')
+open_max = int(parser.get('main', 'open_max'))
 
 print("Read from config file \n\t kafka_topic: {} \n\t open_max: {}").format(kafka_topic, open_max)
 
@@ -26,9 +26,10 @@ consumer = KafkaConsumer(kafka_topic,
                          )
 
 def read_open_level():
-    return 0
+    return int(parser.get('main', 'open_current'))
 
 def set_open_level(level):
+    print("Setting open_current to {}").format(level)
     parser.set('main', 'open_current', str(level))
     #parser.write(sys.stdout)
     with open(parser_file, 'w') as configfile:
@@ -36,14 +37,36 @@ def set_open_level(level):
     return 0
 
 def f_open():
-    print("opening")
+    print("Action called for opening")
+    open_current = read_open_level()
+    print("open_max: {0}\t open_current: {1}\t").format(open_max, open_current)
+    if open_current >= open_max:
+        print("-> (NO) already open to max: {0}").format(open_max)
+    else:
+        print("-> (YES) opening more")
+        set_open_level(open_current + 1) 
 
+    
 def f_close():
-    print("Closing")
+    print("Action called for closing")
+    open_current = read_open_level()
+    print("open_max: {0}\t open_current: {1}\t").format(open_max, open_current)
+    if open_current <= 0:
+        print("-> (NO) already closed and on level: {0}").format(open_current)
+    else:
+        print("-> (YES) closing")
+        set_open_level(open_current - 1) 
+
+# acutal execution
 
 set_open_level(2)
-sleep(10)
-set_open_level(20)
+#open_current = read_open_level()
+#print("Currently we are open: {}").format(open_current)
+#sleep(2)
+#set_open_level(20)
+#open_current = read_open_level()
+#print("Currently we are open: {}").format(open_current)
+
 
 print("Will read from kafka topic: {}").format(kafka_topic)
 for message in consumer:
