462a695def1fbafacfe7d33fa8fdce8c926d0e0f
Remigiusz Boguszewicz <remigiusz.boguszewicz@gmail.com>
2020-05-25 20:55:02 +0200

    a

--- scripto/python/temperature/outside_temperature.py
+++ scripto/python/temperature/outside_temperature.py
@@ -1,16 +1,38 @@
+#!/usr/bin/python
 # coding: utf8
+
+# get temperature readings
+# following: https://rk.edu.pl/pl/pogoda-w-pythonie/
+
 from time import sleep
+from datetime import datetime
 from json import dumps
 from kafka import KafkaProducer
+from ConfigParser import SafeConfigParser
+import pyowm
 
 
-producer = KafkaProducer(bootstrap_servers=['192.168.1.167:9092'],
+print("Reading configuration file")
+parser_file = '/etc/lufcik.ini'
+
+parser = SafeConfigParser()
+parser.read(parser_file)
+
+owm_key = parser.get('main', 'owm_key')
+
+owm = pyowm.OWM(owm_key)  # You MUST provide a valid API key
+
+# Search for current weather in London (Great Britain)
+observation = owm.weather_at_place('Lodz,PL')
+
+producer = KafkaProducer(bootstrap_servers=['sensu:9092'],
                          value_serializer=lambda x: 
                          dumps(x).encode('utf-8'))
 
-for e in range(1000):
-    data = {'number' : e}
-    producer.send('temperature', value=data)
-    producer.send('first_topic', value=data)
-    sleep(1)
+while True:
+    w = observation.get_weather()
+    temp_outside = float(w.get_temperature('celsius')['temp'])
+    #print("{} Sending to kafka topic: temperature_outside, value: {}").format(datetime.now().isoformat(), temp_outside)
+    producer.send('temperature_outside', value=temp_outside)
+    sleep(10)
 
