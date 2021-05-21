1c0a769384fd202a1ff5b15ff8e6912d2b9b8eaf
Remigiusz Boguszewicz <remigiusz.boguszewicz@gmail.com>
2021-05-21 15:21:54 +0200

    a

--- scripto/redis/check_slowlog.sh
+++ scripto/redis/check_slowlog.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-LOG=/tmp/redis_slowlog_`date -I`.txt
-REDISCLI="/usr/local/bin/redis-cli -a hard_pass -p 6379"
+if [ -f ~/.redis_pass ]; then
+  REDIS_PASS=`cat ~/.redis_pass`
+else
+  echo "warning, no password found in ~/.redis_pass"
+  REDIS_PASS="aaa"
+fi
+
+ARCH=`uname -a | awk '{print $(NF-1)}'`
+if [ "$ARCH" = "armv7l" ]; then
+  REDISCLI="/home/pi/tmp/redis/redis-3.2.0/src/redis-cli -a ${REDIS_PASS} -p 6379"
+else
+  REDISCLI="/usr/local/bin/redis-cli -a ${REDIS_PASS} -p 6379"
+fi
 
 REDIS_PID=`ps -ef | grep redis | grep -v sentinel | grep -v grep | awk '{print $2}'`
 echo "REDIS_PID: $REDIS_PID"
@@ -13,10 +24,11 @@ $REDISCLI CONFIG SET slowlog-log-slower-than 10000
 # continuosly write slow commands to log file
 while [ 1 ]
 do
-    date >> $LOG
-    top -b -n 2 -d 0.2 -p $REDIS_PID | tail -2 >> $LOG
-    free -m >> $LOG
-    $REDISCLI info replication >> $LOG
-    $REDISCLI slowlog get 50 >> $LOG
-    sleep 10
+  LOG=/tmp/redis_slowlog_`date -I`.txt
+  date >> $LOG
+  top -b -n 2 -d 0.2 -p $REDIS_PID | tail -2 >> $LOG
+  free -m >> $LOG
+  $REDISCLI info replication >> $LOG
+  $REDISCLI slowlog get 50 >> $LOG
+  sleep 10
 done
