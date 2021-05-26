b6c35720d03e99e2dbc013262f69b1605423fe27
Remigiusz Boguszewicz <remigiusz.boguszewicz@gmail.com>
2021-05-26 16:29:34 +0200

    a

--- scripto/redis/slowlog_summary.sh
+++ scripto/redis/slowlog_summary.sh
@@ -16,10 +16,14 @@ echo "DATE_TO_GREP: $DATE_TO_GREP"
 
 cat $FILE_SLOWLOG | grep "$DATE_TO_GREP" > /tmp/slowlog.tmp1
 
+DATE0=`head -1 $FILE_SLOWLOG`
 # read from dates file two lines
 while read -r DATE1 && read -r DATE2
 do 
   #echo "DATE1=$DATE1 and DATE2=$DATE2"; 
+  LINES_BETWEEN=`sed -n "/$DATE0/,/$DATE1/p" $FILE_SLOWLOG | wc -l`
+  echo "$DATE0 | $LINES_BETWEEN"
   LINES_BETWEEN=`sed -n "/$DATE1/,/$DATE2/p" $FILE_SLOWLOG | wc -l`
   echo "$DATE1 | $LINES_BETWEEN"
+  DATE0=$DATE2
 done </tmp/slowlog.tmp1
