c0b5640be086657a43b465001faab4667e8e85ea
Remigiusz Boguszewicz <remigiusz.boguszewicz@gmail.com>
2021-08-04 13:42:24 +0200

    a

---
+++ scripto/kafka/client_load/load_every_0_1s.py
@@ -0,0 +1,18 @@
+# Read kafka topic where I send all the logs and do the parsing to see it nicely online 
+# with colors and stuff
+
+from time import sleep
+from json import dumps
+from json import loads
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers=['192.168.1.152:9092','192.168.1.153:9092','192.168.1.154:9092'],
+                         value_serializer=lambda x: dumps(x).encode('utf-8'))
+
+for e in range(24*60*60):
+    data = {'number' : e}
+    print(f"sending {data}")
+    producer.send('mm_test1', value=data)
+#    producer.flush()
+    sleep(0.01)
+
