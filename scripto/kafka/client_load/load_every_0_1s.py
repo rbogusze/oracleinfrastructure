a40ffc256fa66793cde710a05d8ac6032d1456f5
Remigiusz Boguszewicz <remigiusz.boguszewicz@gmail.com>
2022-02-10 11:00:18 +0100

    aaa

--- scripto/kafka/client_load/load_every_0_1s.py
+++ scripto/kafka/client_load/load_every_0_1s.py
@@ -5,14 +5,17 @@ from time import sleep
 from json import dumps
 from json import loads
 from kafka import KafkaProducer
+import datetime
 
+today = datetime.date.today()
 producer = KafkaProducer(bootstrap_servers=['192.168.1.152:9092','192.168.1.153:9092','192.168.1.154:9092'],
                          value_serializer=lambda x: dumps(x).encode('utf-8'))
 
 for e in range(24*60*60):
-    data = {'number' : e}
+    data = {'number' : e, 'date': str(today)}
     print(f"sending {data}")
     producer.send('mm_test1', value=data)
 #    producer.flush()
-    sleep(0.01)
+    #sleep(0.1)
+    sleep(1)
 
